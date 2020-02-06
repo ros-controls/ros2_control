45d71acfe73a06da2c88202e4e014beaa72e9e93
Karsten Knese <Karsten1987@users.noreply.github.com>
2020-02-05 16:49:54 -0800

    Dashing update (#47)
    * Add ros2_control metapackage
    
    Signed-off-by: Anas Abou Allaban <aabouallaban@pm.me>
    
    * Add newline and update build type
    
    Signed-off-by: Anas Abou Allaban <allabana@amazon.com>
    
    * Add contributing
    
    Signed-off-by: Anas Abou Allaban <allabana@amazon.com>
    
    * Add License
    
    Signed-off-by: Anas Abou Allaban <aabouallaban@pm.me>
    
    * Add Github actions workflow for testing and linting
    
    Signed-off-by: Anas Abou Allaban <allabana@amazon.com>
    
    * fix master build (#44)
    
    * use std::is_pointer for macro
    * linters
    * use yaml_cpp_vendor
    * add test_robot_hardware to metapackage
    * use rcpputils filesystem helper
    * export yaml-cpp symbols
    
    Co-authored-by: Anas Abou Allaban <aabouallaban@protonmail.com>
    Co-authored-by: Bence Magyar <bence.magyar.robotics@gmail.com>

--- controller_parameter_server/include/controller_parameter_server/yaml_parser.hpp
+++ controller_parameter_server/include/controller_parameter_server/yaml_parser.hpp
@@ -21,7 +21,18 @@
 
 #include "controller_parameter_server/visibility_control.h"
 
+#ifdef _WIN32
+// This is necessary because of a bug in yaml-cpp's cmake
+#define YAML_CPP_DLL
+// This is necessary because yaml-cpp does not always use dllimport/dllexport consistently
+# pragma warning(push)
+# pragma warning(disable:4251)
+# pragma warning(disable:4275)
+#endif
 #include "yaml-cpp/yaml.h"
+#ifdef _WIN32
+# pragma warning(pop)
+#endif
 
 namespace controller_parameter_server
 {
