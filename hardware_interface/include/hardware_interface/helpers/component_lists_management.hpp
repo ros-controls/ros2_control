25b39f428d45b477536647cfe738c9f20fc28e44
Denis Štogl <denis@stogl.de>
2020-08-20 09:16:36 +0200

    Correct faulty header guard

--- hardware_interface/include/hardware_interface/helpers/component_lists_management.hpp
+++ hardware_interface/include/hardware_interface/helpers/component_lists_management.hpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef HARDWARE_INTERFACE__HELPERS__COMPONENTS_LISTS_MANAGEMENT_HPP_
-#define HARDWARE_INTERFACE__HELPERS__COMPONENTS_LISTS_MANAGEMENT_HPP_
+#ifndef HARDWARE_INTERFACE__HELPERS__COMPONENT_LISTS_MANAGEMENT_HPP_
+#define HARDWARE_INTERFACE__HELPERS__COMPONENT_LISTS_MANAGEMENT_HPP_
 
 #include <algorithm>
 #include <string>
@@ -142,4 +142,4 @@ inline return_type set_internal_values(
 
 }  // namespace helpers
 }  // namespace hardware_interface
-#endif  // HARDWARE_INTERFACE__HELPERS__COMPONENTS_LISTS_MANAGEMENT_HPP_
+#endif  // HARDWARE_INTERFACE__HELPERS__COMPONENT_LISTS_MANAGEMENT_HPP_
