c14ae7dd0e81a21b461801d68a637eb6b33129b6
Victor Lopez <victor.lopez@pal-robotics.com>
2020-09-17 10:33:55 +0200

    Add ros2 control cli

---
+++ ros2controlcli/ros2controlcli/verb/unload.py
@@ -0,0 +1,36 @@
+# Copyright 2020 PAL Robotics S.L.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from controller_manager_msgs.srv import LoadController
+import rclpy
+from ros2cli.node.direct import DirectNode
+from ros2cli.node.strategy import add_arguments
+from ros2cli.verb import VerbExtension
+from ros2controlcli.api import unload_controller, LoadedControllerNameCompleter, add_controller_mgr_parsers
+
+
+class UnloadVerb(VerbExtension):
+    """Unload a controller in a controller manager."""
+
+    def add_arguments(self, parser, cli_name):
+        add_arguments(parser)
+        arg = parser.add_argument(
+            'controller_name', help='Name of the controller')
+        arg.completer = LoadedControllerNameCompleter()
+        add_controller_mgr_parsers(parser)
+
+    def main(self, *, args):
+        node_name=args.controller_manager
+        response = unload_controller(args.controller_manager, args.controller_name)
+        return response.ok
---
+++ ros2controlcli/setup.py
@@ -0,0 +1,47 @@
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'ros2controlcli'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+    ],
+    install_requires=['ros2cli'],
+    zip_safe=True,
+    author='Victor Lopez',
+    author_email='victor.lopez@pal-robotics.com',
+    maintainer='Victor Lopez',
+    maintainer_email='victor.lopez@pal-robotics.com',
+    url='https://github.com/ros-controls/ros2_control',
+    keywords=[],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+    ],
+    description='ROS2 Control command  interface.',
+    long_description="""\
+ROS2 Control command  interface.""",
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'ros2cli.command': [
+            'control = ros2controlcli.command.control:ControlCommand',
+        ],
+        'ros2controlcli.verb': [
+            'list = ros2controlcli.verb.list:ListVerb',
+            'list_types = ros2controlcli.verb.list_types:ListTypesVerb',
+            'load = ros2controlcli.verb.load:LoadVerb',
+            'reload_libraries = ros2controlcli.verb.reload_libraries:ReloadLibrariesVerb',
+            'switch = ros2controlcli.verb.switch:SwitchVerb',
+            'unload = ros2controlcli.verb.unload:UnloadVerb',
+        ],
+    }
+)
