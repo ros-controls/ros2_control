833b1661209cc12a9d6f9ef45c87f4072e641aa7
Victor Lopez <victor.lopez@pal-robotics.com>
2020-11-11 17:28:27 +0100

    Fix spurious test failure

--- controller_manager/test/test_controller_manager.cpp
+++ controller_manager/test/test_controller_manager.cpp
@@ -24,6 +24,8 @@
 #include "test_robot_hardware/test_robot_hardware.hpp"
 #include "./test_controller/test_controller.hpp"
 
+using namespace std::chrono_literals;
+
 TEST_F(TestControllerManager, controller_lifecycle) {
   auto cm = std::make_shared<controller_manager::ControllerManager>(
     robot_, executor_,
@@ -134,6 +136,9 @@ TEST_F(TestControllerManager, controller_parameters) {
     std::launch::async, [this]() -> void {
       executor_->spin();
     });
+  // This sleep is needed to prevent a too fast test from ending before the
+  // executor has began to spin, which causes it to hang
+  std::this_thread::sleep_for(50ms);
 
   // controller_manager_parameter because it has no prefix
   // (and controller manager accepts all parameters)
