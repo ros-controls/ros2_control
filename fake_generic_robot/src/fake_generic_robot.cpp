ce9526592132b0a7e42a3c0a95b0370bf84dfada
Denis Stogl <denis.stogl@picknik.ai>
2021-01-18 22:27:48 +0100

    Correct copyright

--- fake_generic_robot/src/fake_generic_robot.cpp
+++ fake_generic_robot/src/fake_generic_robot.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2021, PickNik LLC
+// Copyright (c) 2021 PickNik, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,3 +13,4 @@
 // limitations under the License.
 
 #include "fake_generic_robot/fake_generic_robot.hpp"
+
