2855ab74521b3e90931a72cc6af6146d45a2cbe1
NovusEdge <novusedge0@gmail.com>
2021-03-30 16:15:00 +0530

    Updated format strings

--- ros2controlcli/ros2controlcli/api/__init__.py
+++ ros2controlcli/ros2controlcli/api/__init__.py
@@ -42,13 +42,14 @@ def service_caller(service_name, service_type, request):
             if not cli.wait_for_service(2.0):
                 raise RuntimeError(f'Could not contact service {service_name}')
 
-        node.get_logger().debug('requester: making request: %r\n' % request)
+        node.get_logger().debug(f'requester: making request: {request!r}\n')
         future = cli.call_async(request)
         rclpy.spin_until_future_complete(node, future)
         if future.result() is not None:
             return future.result()
         else:
-            raise RuntimeError('Exception while calling service: %r' % future.exception())
+            future_exception = future.exception()
+            raise RuntimeError(f'Exception while calling service: {future_exception!r}')
     finally:
         node.destroy_node()
         rclpy.shutdown()
