36ee19d3156a6599110193ab818757ac88908590
Tyler Weaver <tylerjw@gmail.com>
2021-04-23 10:16:31 -0600

    Changes to debug macro for Rolling

--- controller_manager/src/controller_manager.cpp
+++ controller_manager/src/controller_manager.cpp
@@ -745,7 +745,7 @@ void ControllerManager::list_controller_types_srv_cb(
   for (const auto & cur_type : cur_types) {
     response->types.push_back(cur_type);
     response->base_classes.push_back(kControllerInterface);
-    RCLCPP_DEBUG(get_logger(), cur_type);
+    RCLCPP_DEBUG_STREAM(get_logger(), cur_type);
   }
 
   RCLCPP_DEBUG(get_logger(), "list types service finished");
