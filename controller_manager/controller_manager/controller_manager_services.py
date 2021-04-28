1a80fc70477fc792e01d7675f9737487012a68e8
Karsten Knese <Karsten1987@users.noreply.github.com>
2021-04-28 15:12:46 -0700

    Bump `wait_for_service` timeout to 10 seconds
    The controller manager might need a few seconds to boot all their hardware resources before the actual `spin` is triggered and its services are available.
    The spawner.py script might be called simultaneously within the same launch file and should thus be able to wait for a bit longer in order for the CM services to be available.

--- controller_manager/controller_manager/controller_manager_services.py
+++ controller_manager/controller_manager/controller_manager_services.py
@@ -25,7 +25,7 @@ def service_caller(node, service_name, service_type, request):
     if not cli.service_is_ready():
         node.get_logger().debug('waiting for service {} to become available...'
                                 .format(service_name))
-        if not cli.wait_for_service(2.0):
+        if not cli.wait_for_service(10.0):
             raise RuntimeError('Could not contact service {}'.format(service_name))
 
     node.get_logger().debug('requester: making request: %r\n' % request)
