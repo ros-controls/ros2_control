18778026632bd28733c5066c9f031df14cf36e16
Denis Štogl <destogl@users.noreply.github.com>
2021-08-20 09:12:17 +0200

    Use example urdf from the test_assests package. (#495)

--- controller_manager/test/controller_manager_test_common.hpp
+++ controller_manager/test/controller_manager_test_common.hpp
@@ -30,102 +30,13 @@
 #include "controller_manager_msgs/srv/switch_controller.hpp"
 
 #include "rclcpp/utilities.hpp"
+
+#include "ros2_control_test_assets/descriptions.hpp"
 #include "test_controller/test_controller.hpp"
 #include "test_controller_failed_init/test_controller_failed_init.hpp"
 
 constexpr auto STRICT = controller_manager_msgs::srv::SwitchController::Request::STRICT;
 constexpr auto BEST_EFFORT = controller_manager_msgs::srv::SwitchController::Request::BEST_EFFORT;
-
-namespace controller_manager_test
-{
-constexpr auto urdf =
-  R"(
-<?xml version="1.0" encoding="utf-8"?>
-<robot name="MinimalRobot">
-  <joint name="base_joint" type="fixed">
-    <origin rpy="0 0 0" xyz="0 0 0"/>
-    <parent link="world"/>
-    <child link="base_link"/>
-  </joint>
-  <link name="base_link">
-    <collision>
-      <origin rpy="0 0 0" xyz="0 0 0"/>
-      <geometry>
-        <cylinder length="1" radius="0.1"/>
-      </geometry>
-    </collision>
-  </link>
-  <joint name="joint1" type="revolute">
-    <origin rpy="-1.57079632679 0 0" xyz="0 0 0.2"/>
-    <parent link="base_link"/>
-    <child link="link1"/>
-    <limit effort="0.1" lower="-3.14159265359" upper="3.14159265359" velocity="0.2"/>
-  </joint>
-  <link name="link1">
-    <collision>
-      <origin rpy="0 0 0" xyz="0 0 0"/>
-      <geometry>
-        <cylinder length="1" radius="0.1"/>
-      </geometry>
-    </collision>
-  </link>
-  <joint name="joint2" type="revolute">
-    <origin rpy="1.57079632679 0 0" xyz="0 0 0.9"/>
-    <parent link="link1"/>
-    <child link="link2"/>
-    <limit effort="0.1" lower="-3.14159265359" upper="3.14159265359" velocity="0.2"/>
-  </joint>
-  <link name="link2">
-    <collision>
-      <origin rpy="0 0 0" xyz="0 0 0"/>
-      <geometry>
-        <cylinder length="1" radius="0.1"/>
-      </geometry>
-    </collision>
-  </link>
-  <joint name="tool_joint" type="fixed">
-    <origin rpy="0 0 0" xyz="0 0 1"/>
-    <parent link="link2"/>
-    <child link="tool_link"/>
-  </joint>
-  <ros2_control name="TestActuatorHardware" type="actuator">
-    <hardware>
-      <plugin>test_actuator</plugin>
-    </hardware>
-    <joint name="joint1">
-      <command_interface name="position"/>
-      <state_interface name="position"/>
-      <state_interface name="velocity"/>
-    </joint>
-  </ros2_control>
-  <ros2_control name="TestSensorHardware" type="sensor">
-    <hardware>
-      <plugin>test_sensor</plugin>
-      <param name="example_param_write_for_sec">2</param>
-      <param name="example_param_read_for_sec">2</param>
-    </hardware>
-    <sensor name="sensor1">
-      <state_interface name="velocity"/>
-    </sensor>
-  </ros2_control>
-  <ros2_control name="TestSystemHardware" type="system">
-    <hardware>
-      <plugin>test_system</plugin>
-      <param name="example_param_write_for_sec">2</param>
-      <param name="example_param_read_for_sec">2</param>
-    </hardware>
-    <joint name="joint2">
-      <command_interface name="velocity"/>
-      <state_interface name="position"/>
-    </joint>
-    <joint name="joint3">
-      <command_interface name="velocity"/>
-      <state_interface name="position"/>
-    </joint>
-  </ros2_control>
-</robot>
-)";
-}  // namespace controller_manager_test
 
 class ControllerManagerFixture : public ::testing::Test
 {
@@ -144,7 +55,9 @@ public:
   {
     executor_ = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
     cm_ = std::make_shared<controller_manager::ControllerManager>(
-      std::make_unique<hardware_interface::ResourceManager>(controller_manager_test::urdf),
+      std::make_unique<hardware_interface::ResourceManager>(
+        ros2_control_test_assets::
+        minimal_robot_urdf),
       executor_, "test_controller_manager");
     run_updater_ = false;
   }
