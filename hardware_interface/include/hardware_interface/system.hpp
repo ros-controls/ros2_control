c0552bca9f46bed32f0352b91373143b9cbcc8f6
Denis Štogl <denis@stogl.de>
2021-08-27 11:05:10 +0200

    Update copyright year because of substantial changes right now.

--- hardware_interface/include/hardware_interface/system.hpp
+++ hardware_interface/include/hardware_interface/system.hpp
@@ -1,4 +1,4 @@
-// Copyright 2020 ros2_control Development Team
+// Copyright 2021 ros2_control Development Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
