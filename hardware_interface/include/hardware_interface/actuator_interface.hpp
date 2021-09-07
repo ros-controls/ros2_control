895e249def0d1bef3f0913607681ebba2374af80
Denis Štogl <denis@stogl.de>
2021-09-07 11:58:47 +0200

    Update ActuatorInterface according to comments.

--- hardware_interface/include/hardware_interface/actuator_interface.hpp
+++ hardware_interface/include/hardware_interface/actuator_interface.hpp
@@ -22,6 +22,8 @@
 #include "hardware_interface/handle.hpp"
 #include "hardware_interface/hardware_info.hpp"
 #include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "hardware_interface/types/lifecycle_state_names.hpp"
+#include "lifecycle_msgs/msg/state.hpp"
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
 #include "rclcpp_lifecycle/state.hpp"
 
@@ -32,11 +34,44 @@ namespace hardware_interface
 /// Virtual Class to implement when integrating a 1 DoF actuator into ros2_control.
 /**
   * The typical examples are conveyors or motors.
+  *
+  * Methods return values have type rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn with the following meaning:
+  *
+  * \returns CallbackReturn::SUCCESS method execution was successful.
+  * \returns CallbackReturn::FAILURE method execution has failed and and can be called again.
+  * \returns CallbackReturn::ERROR critical error has happened that should be managed in
+  * "on_error" method.
+  *
+  * The hardware ends after each method in a state with the following meaning:
+  *
+  * UNCONFIGURED (on_init, on_cleanup):
+  *   Hardware is initialized but communication is not started and therefore no interface is available.
+  *
+  * INACTIVE (on_configure, on_deactivate):
+  *   Communication with the hardware is started and it is configured.
+  *   States can be read and non-movement hardware interfaces commanded.
+  *   Hardware interfaces for movement will NOT be available.
+  *   Those interfaces are: HW_IF_POSITION, HW_IF_VELOCITY, HW_IF_ACCELERATION, and HW_IF_EFFORT.
+  *
+  * FINALIZED (on_shutdown):
+  *   Hardware interface is ready for unloading/destruction.
+  *   Allocated memory is cleaned up.
+  *
+  * ACTIVE (on_activate):
+  *   Power circuits of hardware are active and hardware can be moved, e.g., brakes are disabled.
+  *   Command interfaces for movement are available and have to be accepted.
+  *   Those interfaces are: HW_IF_POSITION, HW_IF_VELOCITY, HW_IF_ACCELERATION, and HW_IF_EFFORT.
+  *
+  *
   */
-class ActuatorInterface
+class ActuatorInterface : public rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface
 {
 public:
-  ActuatorInterface() = default;
+  ActuatorInterface()
+  : lifecycle_state_(rclcpp_lifecycle::State(
+      lifecycle_msgs::msg::State::PRIMARY_STATE_UNKNOWN, lifecycle_state_names::UNKNOWN))
+  {
+  }
 
   virtual ~ActuatorInterface() = default;
 
@@ -46,100 +81,11 @@ public:
    * \returns CallbackReturn::SUCCESS if required data are provided and can be parsed.
    * \returns CallbackReturn::ERROR if any error happens or data are missing.
    */
-  virtual CallbackReturn on_init(const HardwareInfo & actuator_info) = 0;
-
-  /// Configuration of the hardware and start communication with it.
-  /**
-   * Configuration of the hardware. It start communication with hardware and configures it.
-   * After this method finishes states can be read, and non-movement hardware interfaces commanded.
-   *
-   * Hardware interfaces for movement (will NOT be available) are:
-   * HW_IF_POSITION, HW_IF_VELOCITY, HW_IF_ACCELERATION, and HW_IF_EFFORT.
-   *
-   * \returns LifecycleNodeInterface::CallbackReturn::SUCCESS if configuration is successful.
-   * \returns LifecycleNodeInterface::CallbackReturn::FAILURE if configuration fails and can be
-   * tried again.
-   * \returns LifecycleNodeInterface::CallbackReturn::ERROR critical error has happened that
-   * should be managed in "on_error" method.
-   */
-  virtual CallbackReturn on_configure() { return CallbackReturn::SUCCESS; }
-
-  /// Cleanup hardware configuration and stop communication with it.
-  /**
-   * Cleanup of the hardware. Communication with hardware is stopped.
-   * Interfaces are not available anymore.
-   *
-   * \returns LifecycleNodeInterface::CallbackReturn::SUCCESS if everything worked as expected.
-   * \returns LifecycleNodeInterface::CallbackReturn::FAILURE if cleanup fails and can be
-   * tried again.
-   * \returns LifecycleNodeInterface::CallbackReturn::ERROR critical error has happened that
-   * should be managed in "on_error" method.
-   */
-  virtual CallbackReturn on_cleanup() { return CallbackReturn::SUCCESS; }
-
-  /// Shutdown hardware interface and prepare plugin for destruction.
-  /**
-   * Shutdown hardware interface.
-   * Cleanup memory allocations and prepare plugin for clean unloading/destruction.
-   *
-   * \param[in] previous_state state from which the method is called.
-   * \returns LifecycleNodeInterface::CallbackReturn::SUCCESS if everything worked as expected.
-   * \returns LifecycleNodeInterface::CallbackReturn::ERROR critical error has happened that
-   * should be managed in "on_error" method.
-   */
-  virtual CallbackReturn on_shutdown(const rclcpp_lifecycle::State & /*previous_state*/)
+  virtual CallbackReturn on_init(const HardwareInfo & actuator_info)
   {
+    info_ = actuator_info;
     return CallbackReturn::SUCCESS;
-  }
-
-  /// Activate hardware power and enable its movement.
-  /**
-   * Activate power circuits of the hardware and prepare everything for commanding movements, e.g.,
-   * disable brakes.
-   * Command interfaces for movement has to be available after this and movement commands have to
-   * be accepted.
-   *
-   * Hardware interfaces for movement are:
-   * HW_IF_POSITION, HW_IF_VELOCITY, HW_IF_ACCELERATION, and HW_IF_EFFORT.
-   *
-   * \returns LifecycleNodeInterface::CallbackReturn::SUCCESS if everything worked as expected.
-   * \returns LifecycleNodeInterface::CallbackReturn::FAILURE if activation fails and can be
-   * tried again.
-   * \returns LifecycleNodeInterface::CallbackReturn::ERROR critical error has happened that
-   * should be managed in "on_error" method.
-   */
-  virtual CallbackReturn on_activate() = 0;
-
-  /// Deactivate hardware power and disable its movement.
-  /**
-   * Deactivate power circuits of the hardware and disable possibility to move, e.g., activate
-   * breaks.
-   * Command interface for movement are disabled and movement commands are ignored
-   *
-   * Hardware interfaces for movement are:
-   * HW_IF_POSITION, HW_IF_VELOCITY, HW_IF_ACCELERATION, and HW_IF_EFFORT.
-   *
-   * \returns LifecycleNodeInterface::CallbackReturn::SUCCESS if everything worked as expected.
-   * \returns LifecycleNodeInterface::CallbackReturn::FAILURE if deactivation fails and can be
-   * tried again.
-   * \returns LifecycleNodeInterface::CallbackReturn::ERROR critical error has happened that
-   * should be managed in "on_error" method.
-   */
-  virtual CallbackReturn on_deactivate() = 0;
-
-  /// Deal with critical errors that happen on state changes.
-  /**
-   * Deal with critical errors that happen on state changes.
-   * The method is called from any state.
-   *
-   * \param[in] previous_state state from which the method is called.
-   * \returns LifecycleNodeInterface::CallbackReturn::SUCCESS if everything worked as expected.
-   * \returns LifecycleNodeInterface::CallbackReturn::FAILURE if any error happens.
-   */
-  virtual CallbackReturn on_error(const rclcpp_lifecycle::State & /*previous_state*/)
-  {
-    return CallbackReturn::SUCCESS;
-  }
+  };
 
   /// Exports all state interfaces for this actuator.
   /**
@@ -206,7 +152,13 @@ public:
   /**
    * \return name.
    */
-  virtual std::string get_name() const = 0;
+  std::string get_name() const { return info_.name; }
+
+  /// Get life-cycle state of the actuator hardware.
+  /**
+   * \return state.
+   */
+  const rclcpp_lifecycle::State & get_state() const { return lifecycle_state_; }
 
   /// Read the current state values from the actuator.
   /**
@@ -226,6 +178,10 @@ public:
    * \return return_type::OK if the read was successful, return_type::ERROR otherwise.
    */
   virtual return_type write() = 0;
+
+private:
+  HardwareInfo info_;
+  rclcpp_lifecycle::State lifecycle_state_;
 };
 
 }  // namespace hardware_interface
