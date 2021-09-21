11058aa5349e9c9db949c3cb8426fe02d03835c5
Denis Štogl <denis@stogl.de>
2021-09-21 14:45:47 +0200

    Comment out unused variables to supress warnings.

--- controller_manager/test/test_controller/test_controller.cpp
+++ controller_manager/test/test_controller/test_controller.cpp
@@ -28,7 +28,7 @@ TestController::TestController()
 }
 
 controller_interface::return_type TestController::update(
-  const rclcpp::Time & time, const rclcpp::Duration & period)
+  const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
 {
   ++internal_counter;
   return controller_interface::return_type::OK;
