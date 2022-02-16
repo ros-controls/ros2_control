f5ef068bea6171c46fb1ae5820312098eabdc190
Márk Szitanics <szitanics@gmail.com>
2022-02-16 13:54:00 +0100

    Port transmission loader plugins from ROS1 (#633)
    * Add simple transmission loader (in progress)
    * Rewrite the test cases for it based on the ROS Noetic version
    * Add working tests for simple transmission loader
    
    Co-authored-by: Bence Magyar <bence.magyar.robotics@gmail.com>
    Co-authored-by: Denis Štogl <destogl@users.noreply.github.com>

---
+++ transmission_interface/include/transmission_interface/transmission_loader.hpp
@@ -0,0 +1,40 @@
+// Copyright 2022 PAL Robotics S.L.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TRANSMISSION_INTERFACE__TRANSMISSION_LOADER_HPP_
+#define TRANSMISSION_INTERFACE__TRANSMISSION_LOADER_HPP_
+
+#include <memory>
+
+#include "hardware_interface/hardware_info.hpp"
+
+namespace transmission_interface
+{
+/**
+ * \brief Abstract interface for loading transmission instances from configuration data.
+ *
+ * It also provides convenience methods for specific transmission loaders to leverage.
+ */
+class TransmissionLoader
+{
+public:
+  virtual ~TransmissionLoader() = default;
+
+  virtual std::shared_ptr<Transmission> load(
+    const hardware_interface::TransmissionInfo & transmission_info) = 0;
+};
+
+}  // namespace transmission_interface
+
+#endif  // TRANSMISSION_INTERFACE__TRANSMISSION_LOADER_HPP_
---
+++ transmission_interface/src/simple_transmission_loader.cpp
@@ -0,0 +1,51 @@
+// Copyright 2022 PAL Robotics S.L.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "transmission_interface/simple_transmission_loader.hpp"
+
+#include <memory>
+
+#include "hardware_interface/component_parser.hpp"
+#include "hardware_interface/hardware_info.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+#include "pluginlib/class_list_macros.hpp"
+#include "rclcpp/logging.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "transmission_interface/simple_transmission.hpp"
+#include "transmission_interface/transmission_interface_exception.hpp"
+
+namespace transmission_interface
+{
+std::shared_ptr<Transmission> SimpleTransmissionLoader::load(
+  const hardware_interface::TransmissionInfo & transmission_info)
+{
+  try
+  {
+    std::shared_ptr<Transmission> transmission(new SimpleTransmission(
+      transmission_info.joints[0].mechanical_reduction, transmission_info.joints[0].offset));
+    return transmission;
+  }
+  catch (const transmission_interface::TransmissionInterfaceException & ex)
+  {
+    RCLCPP_ERROR(
+      rclcpp::get_logger("simple_transmission_loader"), "Failed to construct transmission '%s'",
+      ex.what());
+    return std::shared_ptr<Transmission>();
+  }
+}
+
+}  // namespace transmission_interface
+
+PLUGINLIB_EXPORT_CLASS(
+  transmission_interface::SimpleTransmissionLoader, transmission_interface::TransmissionLoader)
