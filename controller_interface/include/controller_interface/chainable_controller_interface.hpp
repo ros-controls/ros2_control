5388d88824f3819a557e35229fe2118feeed364f
Tyler Weaver <tylerjw@gmail.com>
2022-06-12 11:18:23 -0600

    Add doxygen comments
    Signed-off-by: Tyler Weaver <tylerjw@gmail.com>

--- controller_interface/include/controller_interface/chainable_controller_interface.hpp
+++ controller_interface/include/controller_interface/chainable_controller_interface.hpp
@@ -40,6 +40,13 @@ public:
   CONTROLLER_INTERFACE_PUBLIC
   virtual ~ChainableControllerInterface() = default;
 
+  /**
+   * This is the method called in the control loop.
+   *
+   * \param[in] time The time at the start of this control loop iteration
+   * \param[in] period The measured time taken by the last control loop iteration
+   * \returns return_type::OK if update is successfully, otherwise return_type::ERROR.
+   */
   CONTROLLER_INTERFACE_PUBLIC
   return_type update(const rclcpp::Time & time, const rclcpp::Duration & period) final;
 
