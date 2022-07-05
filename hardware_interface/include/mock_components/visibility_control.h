54b7f0b255470f62512eee7a58e4f8a927325fd8
Bence Magyar <bence.magyar.robotics@gmail.com>
2022-07-05 17:13:07 +0100

    fix cpplint

--- hardware_interface/include/mock_components/visibility_control.h
+++ hardware_interface/include/mock_components/visibility_control.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef FAKE_COMPONENTS__VISIBILITY_CONTROL_H_
-#define FAKE_COMPONENTS__VISIBILITY_CONTROL_H_
+#ifndef MOCK_COMPONENTS__VISIBILITY_CONTROL_H_
+#define MOCK_COMPONENTS__VISIBILITY_CONTROL_H_
 
 // This logic was borrowed (then namespaced) from the examples on the gcc wiki:
 //     https://gcc.gnu.org/wiki/Visibility
@@ -46,4 +46,4 @@
 #define MOCK_COMPONENTS_PUBLIC_TYPE
 #endif
 
-#endif  // FAKE_COMPONENTS__VISIBILITY_CONTROL_H_
+#endif  // MOCK_COMPONENTS__VISIBILITY_CONTROL_H_
