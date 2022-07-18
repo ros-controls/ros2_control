8e6bde11fde9f7ea969b1728a34bd3ad3d8db778
Paul Gesel <paul.gesel@picknik.ai>
2022-07-18 16:52:29 -0600

    added test for visualization

--- ros2controlcli/ros2controlcli/verb/view_controller_chains.py
+++ ros2controlcli/ros2controlcli/verb/view_controller_chains.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 from controller_manager import list_controllers
+from controller_manager import list_hardware_interfaces
 
 from ros2cli.node.direct import add_arguments
 from ros2cli.node.strategy import NodeStrategy
@@ -23,86 +24,127 @@ from ros2controlcli.api import add_controller_mgr_parsers
 import graphviz
 
 
-def make_controller_node(s, controller_name, inputs, outputs, port_map):
-    inputs = sorted(list(inputs))
-    outputs = sorted(list(outputs))
-    inputs_str = ''
-    outputs_str = ''
-    for ind, input in enumerate(inputs):
-        deliminator = '|'
-        if ind == len(inputs) - 1:
-            deliminator = ''
-        input = input.replace('/', '_')
-        name = input
-        inputs_str += '<%s> %s %s ' % ("input_" + input, name, deliminator)
-        port_map["input_" + input] = controller_name
+# controller_name, state_connections[controller_name], command_connections[controller_name],
+# input_chain_connections[controller_name], output_chain_connections
 
-    for ind, output in enumerate(outputs):
+def make_controller_node(s, controller_name, state_interfaces, command_interfaces, input_controllers,
+                         output_controllers, port_map):
+    state_interfaces = sorted(list(state_interfaces))
+    command_interfaces = sorted(list(command_interfaces))
+    input_controllers = sorted(list(input_controllers))
+    output_controllers = sorted(list(output_controllers))
+
+    inputs_str = ''
+    for ind, state_interface in enumerate(state_interfaces):
         deliminator = '|'
-        if ind == len(outputs) - 1:
+        if ind == len(state_interface) - 1:
             deliminator = ''
-        output = output.replace('/', '_')
-        outputs_str += '<%s> %s %s ' % ("output_" + output, '', deliminator)
-        port_map["output_" + output] = controller_name
+        inputs_str += '<%s> %s %s ' % ("state_end_" + state_interface, state_interface, deliminator)
+
+    for ind, input_controller in enumerate(input_controllers):
+        deliminator = '|'
+        if ind == len(input_controller) - 1:
+            deliminator = ''
+        inputs_str += '<%s> %s %s ' % ("controller_end_" + input_controller, input_controller, deliminator)
+        port_map["controller_end_" + input_controller] = controller_name
+
+    outputs_str = ''
+    for ind, command_interface in enumerate(command_interfaces):
+        deliminator = '|'
+        if ind == len(command_interface) - 1:
+            deliminator = ''
+        outputs_str += '<%s> %s %s ' % ("command_start_" + command_interface, command_interface, deliminator)
+
+    for ind, output_controller in enumerate(output_controllers):
+        deliminator = '|'
+        if ind == len(output_controller) - 1:
+            deliminator = ''
+        outputs_str += '<%s> %s %s ' % ("controller_start_" + output_controller, output_controller, deliminator)
 
     s.node(controller_name, '%s|{{%s}|{%s}}' % (controller_name, inputs_str, outputs_str))
 
 
-def make_command_node(s, output_interfaces, port_map):
-    outputs = sorted(list(output_interfaces))
+def make_command_node(s, command_interfaces):
+    command_interfaces = sorted(list(command_interfaces))
     outputs_str = ''
-    for ind, output in enumerate(outputs):
+    for ind, command_interface in enumerate(command_interfaces):
         deliminator = '|'
-        if ind == len(outputs) - 1:
+        if ind == len(command_interfaces) - 1:
             deliminator = ''
-        output = output.replace('/', '_')
-        name = output
-        outputs_str += '<%s> %s %s ' % ("input_" + output, name, deliminator)
-        port_map["input_" + output] = "command_interfaces"
+        outputs_str += '<%s> %s %s ' % ("command_end_" + command_interface, command_interface, deliminator)
 
     s.node("command_interfaces", '%s|{{%s}}' % ("command_interfaces", outputs_str))
 
 
-def make_state_node(s, input_interfaces, port_map):
-    inputs = sorted(list(input_interfaces))
+def make_state_node(s, state_interfaces):
+    state_interfaces = sorted(list(state_interfaces))
     inputs_str = ''
-    for ind, input in enumerate(inputs):
+    for ind, state_interface in enumerate(state_interfaces):
         deliminator = '|'
-        if ind == len(inputs) - 1:
+        if ind == len(state_interfaces) - 1:
             deliminator = ''
-        input = input.replace('/', '_')
-        name = input
-        inputs_str += '<%s> %s %s ' % ("output_" + input, name, deliminator)
-        port_map["output_" + input] = "state_interfaces"
+        inputs_str += '<%s> %s %s ' % ("state_start_" + state_interface, state_interface, deliminator)
 
     s.node("state_interfaces", '%s|{{%s}}' % ("state_interfaces", inputs_str))
 
 
-def show_graph(output_connections, input_connections, output_interfaces, input_interfaces):
+def show_graph(input_chain_connections, output_chain_connections, command_connections, state_connections,
+               command_interfaces, state_interfaces):
     s = graphviz.Digraph('g', filename='/tmp/controller_diagram.gv', node_attr={'shape': 'record', 'style': 'rounded'})
     port_map = dict()
-    for controller_name in input_connections:
-        make_controller_node(s, controller_name, input_connections[controller_name],
-                             output_connections[controller_name], port_map)
+    # get all controller names
+    controller_names = set()
+    controller_names = controller_names.union(set([name for name in input_chain_connections]))
+    controller_names = controller_names.union(set([name for name in output_chain_connections]))
+    controller_names = controller_names.union(set([name for name in command_connections]))
+    controller_names = controller_names.union(set([name for name in state_connections]))
+    # create node for each controller
+    for controller_name in controller_names:
+        make_controller_node(s, controller_name, state_connections[controller_name],
+                             command_connections[controller_name],
+                             input_chain_connections[controller_name],
+                             output_chain_connections[controller_name], port_map)
 
-    make_state_node(s, input_interfaces, port_map)
-    make_command_node(s, output_interfaces, port_map)
+    make_state_node(s, state_interfaces)
+    make_command_node(s, command_interfaces)
 
-    for controller_name in output_connections:
-        for connection in output_connections[controller_name]:
-            connection = connection.replace('/', '_')
-            s.edge('%s:%s' % (controller_name, "output_" + connection),
-                   '%s:%s' % (port_map['input_' + connection], 'input_' + connection))
-        for connection in input_connections[controller_name]:
-            if connection in input_interfaces:
-                connection = connection.replace('/', '_')
-                s.edge('%s:%s' % ("state_interfaces", "output_" + connection),
-                       '%s:%s' % (controller_name, 'input_' + connection))
+    for controller_name in controller_names:
+        for connection in output_chain_connections[controller_name]:
+            s.edge('%s:%s' % (controller_name, "controller_start_" + connection),
+                   '%s:%s' % (port_map['controller_end_' + connection], 'controller_end_' + connection))
+        for state_connection in state_connections[controller_name]:
+            s.edge('%s:%s' % ("state_interfaces", "state_start_" + state_connection),
+                   '%s:%s' % (controller_name, 'state_end_' + state_connection))
+        for command_connection in command_connections[controller_name]:
+            s.edge('%s:%s' % (controller_name, "command_start_" + command_connection),
+                   '%s:%s' % ("command_interfaces", 'command_end_' + command_connection))
 
-    s.attr(splines="false")
+    # s.attr(splines="false")
     s.attr(ranksep='2')
     s.attr(rankdir="LR")
     s.view()
+
+
+def parse_response(list_controllers_response, list_hardware_response):
+    command_interfaces = set([x.name for x in list_hardware_response.command_interfaces])
+    state_interfaces = set([x.name for x in list_hardware_response.state_interfaces])
+    command_connections = dict()
+    state_connections = dict()
+    input_chain_connections = {x.name: set() for x in list_controllers_response.controller}
+    output_chain_connections = {x.name: set() for x in list_controllers_response.controller}
+
+    for controller in list_controllers_response.controller:
+        for chain_connection in controller.chain_connections:
+            for reference_interface in chain_connection.reference_interfaces:
+                output_chain_connections[controller.name].add(reference_interface)
+        for reference_interface in controller.reference_interfaces:
+            input_chain_connections[controller.name].add(reference_interface)
+
+        command_connections[controller.name] = set(controller.required_command_interfaces)
+        state_connections[controller.name] = set(controller.required_state_interfaces)
+
+    show_graph(input_chain_connections, output_chain_connections, command_connections, state_connections,
+               command_interfaces, state_interfaces)
 
 
 class ViewControllerChainsVerb(VerbExtension):
@@ -114,33 +156,7 @@ class ViewControllerChainsVerb(VerbExtension):
 
     def main(self, *, args):
         with NodeStrategy(args) as node:
-            response = list_controllers(node, args.controller_manager)
-            output_interfaces = set()
-            input_interfaces = set()
-            output_connections = dict()
-            input_connections = dict()
-            for c_chained in response.controller_group:
-                state_interfaces = set(c_chained.controller_state.required_state_interfaces)
-                required_interfaces = set(c_chained.controller_state.required_command_interfaces)
-                chained_interfaces = set()
-                if len(c_chained.chained_connections) > 0:
-                    chained_interfaces = set([y for x in c_chained.chained_connections for y in x.reference_interfaces])
-                    output_connections[c_chained.controller_state.name] = chained_interfaces
-                else:
-                    output_connections[c_chained.controller_state.name] = required_interfaces
-
-                input_interfaces = input_interfaces.union(state_interfaces)
-                output_interfaces = output_interfaces.union(required_interfaces - chained_interfaces)
-                input_connections[c_chained.controller_state.name] = state_interfaces
-
-            for controller_name in output_connections:
-                connections = output_connections[controller_name]
-                for connection in connections:
-                    connection_split = connection.split('/')
-                    if len(connection_split) > 0:
-                        prefix = connection_split[0]
-                        if prefix in input_connections:
-                            input_connections[prefix].add(connection)
-
-            show_graph(output_connections, input_connections, output_interfaces, input_interfaces)
+            list_controllers_response = list_controllers(node, args.controller_manager)
+            list_hardware_response = list_hardware_interfaces(node, args.controller_manager)
+            parse_response(list_controllers_response, list_hardware_response)
             return 0
---
+++ ros2controlcli/test/test_view_controller_chains.py
@@ -0,0 +1,66 @@
+import unittest
+from controller_manager_msgs.msg import ControllerState
+from controller_manager_msgs.msg import HardwareInterface
+from controller_manager_msgs.msg import ChainConnection
+from controller_manager_msgs.srv import ListControllers
+from controller_manager_msgs.srv import ListHardwareInterfaces
+
+from ros2controlcli.verb.view_controller_chains import parse_response
+
+
+class TestViewControllerChains(unittest.TestCase):
+
+    def test_expected(self):
+
+        list_controllers_response = ListControllers.Response()
+        list_hardware_response = ListHardwareInterfaces.Response()
+
+        chained_to_controller = ControllerState()
+        chained_from_controller = ControllerState()
+        chain_connection = ChainConnection()
+
+        state_interfaces = []
+        command_interfaces = []
+
+        chained_to_controller.name = "chained_controller"
+        chained_to_controller.state = "active"
+        chained_from_controller.name = "controller"
+        chained_from_controller.state = "active"
+        chain_connection.name = chained_to_controller.name
+
+        for i in range(1, 7):
+            pos_int = HardwareInterface(name=f"joint{i}/position")
+            vel_int = HardwareInterface(name=f"joint{i}/velocity")
+            state_interfaces.append(pos_int)
+            state_interfaces.append(vel_int)
+            command_interfaces.append(pos_int)
+            command_interfaces.append(vel_int)
+
+            chained_to_controller.claimed_interfaces.append(f"joint{i}/position")
+            chained_to_controller.claimed_interfaces.append(f"joint{i}/velocity")
+            chained_to_controller.required_state_interfaces.append(f"joint{i}/position")
+            chained_to_controller.reference_interfaces.append(f"joint{i}/position")
+            chained_to_controller.reference_interfaces.append(f"joint{i}/velocity")
+
+            # chained_from_controller.claimed_interfaces.append(f"{chained_to_controller.name}/joint{i}/position")
+            # chained_from_controller.claimed_interfaces.append(f"{chained_to_controller.name}/joint{i}/velocity")
+            chained_from_controller.required_state_interfaces.append(f"joint{i}/position")
+            chained_from_controller.required_state_interfaces.append(f"joint{i}/velocity")
+
+            chain_connection.reference_interfaces.append(f"joint{i}/position")
+            chain_connection.reference_interfaces.append(f"joint{i}/velocity")
+
+        chained_to_controller.required_command_interfaces = chained_to_controller.claimed_interfaces
+
+        chained_from_controller.required_command_interfaces = chained_from_controller.claimed_interfaces
+        chained_from_controller.chain_connections.append(chain_connection)
+
+        controller_list = [chained_from_controller, chained_to_controller]
+
+        list_controllers_response.controller = controller_list
+        list_hardware_response.state_interfaces = state_interfaces
+        list_hardware_response.command_interfaces = command_interfaces
+        try:
+            parse_response(list_controllers_response, list_hardware_response)
+        except Exception as e:
+            self.assertTrue(0, "parse_response failed!")
