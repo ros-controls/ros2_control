ros-controls/ros2_control

commit abd5d6e6c8264706b58dddc877063f884cf34a58

Deprecate and rename `start` and `stop` nomenclature toward user to `activate` and `deactivate` #ABI-breaking (#755)
* Rename fileds and deprecated old nomenclature.

* Remove new defines from SwitchController.srv

* Deprecated start/stop nomenclature in all CLI commands.

* Deprecate 'start_asap' too as other fields.

controller_manager/controller_manager/spawner.py
@@ -133,7 +133,10 @@ def main(args=None):
         '--load-only', help='Only load the controller and leave unconfigured.',
         action='store_true', required=False)
     parser.add_argument(
-        '--stopped', help='Load and configure the controller, however do not start them',
+        '--stopped', help='Load and configure the controller, however do not activate them',
+        action='store_true', required=False)
+    parser.add_argument(
+        '--inactive', help='Load and configure the controller, however do not activate them',
         action='store_true', required=False)
     parser.add_argument(
         '-t', '--controller-type',
@@ -193,7 +196,7 @@ def main(args=None):
                 node.get_logger().info('Failed to configure controller')
                 return 1
 
-            if not args.stopped:
+            if not args.stopped and not args.inactive:
                 ret = switch_controllers(
                     node,
                     controller_manager_name,
@@ -203,11 +206,13 @@ def main(args=None):
                     True,
                     5.0)
                 if not ret.ok:
-                    node.get_logger().info('Failed to start controller')
+                    node.get_logger().info('Failed to activate controller')
                     return 1
 
-                node.get_logger().info(bcolors.OKGREEN + 'Configured and started ' +
+                node.get_logger().info(bcolors.OKGREEN + 'Configured and activated ' +
                                        bcolors.OKCYAN + controller_name + bcolors.ENDC)
+            elif args.stopped:
+                node.get_logger.warn('"--stopped" flag is deprecated use "--inactive" instead')
 
         if not args.unload_on_kill:
             return 0
@@ -217,8 +222,8 @@ def main(args=None):
             while True:
                 time.sleep(1)
         except KeyboardInterrupt:
-            if not args.stopped:
-                node.get_logger().info('Interrupt captured, stopping and unloading controller')
+            if not args.stopped and not args.inactive:
+                node.get_logger().info('Interrupt captured, deactivating and unloading controller')
                 ret = switch_controllers(
                     node,
                     controller_manager_name,
@@ -228,10 +233,13 @@ def main(args=None):
                     True,
                     5.0)
                 if not ret.ok:
-                    node.get_logger().info('Failed to stop controller')
+                    node.get_logger().info('Failed to deactivate controller')
                     return 1
 
-                node.get_logger().info('Stopped controller')
+                node.get_logger().info('Deactivated controller')
+
+            elif args.stopped:
+                node.get_logger.warn('"--stopped" flag is deprecated use "--inactive" instead')
 
             ret = unload_controller(
                 node, controller_manager_name, controller_name)

controller_manager/controller_manager/controller_manager_services.py
@@ -78,16 +78,16 @@ def reload_controller_libraries(node, controller_manager_name, force_kill):
                           ReloadControllerLibraries, request)
 
 
-def switch_controllers(node, controller_manager_name, stop_controllers,
-                       start_controllers, strict, start_asap, timeout):
+def switch_controllers(node, controller_manager_name, deactivate_controllers,
+                       activate_controllers, strict, activate_asap, timeout):
     request = SwitchController.Request()
-    request.start_controllers = start_controllers
-    request.stop_controllers = stop_controllers
+    request.activate_controllers = activate_controllers
+    request.deactivate_controllers = deactivate_controllers
     if strict:
         request.strictness = SwitchController.Request.STRICT
     else:
         request.strictness = SwitchController.Request.BEST_EFFORT
-    request.start_asap = start_asap
+    request.activate_asap = activate_asap
     request.timeout = rclpy.duration.Duration(seconds=timeout).to_msg()
     return service_caller(node, f'{controller_manager_name}/switch_controller',
                           SwitchController, request)

controller_manager/controller_manager/launch_utils.py
@@ -27,7 +27,7 @@ def generate_load_controller_launch_description(controller_name,
 
     Returns a list of LaunchDescription actions adding the 'controller_manager_name' and
     'unload_on_kill' LaunchArguments and a Node action that runs the controller_manager
-    spawner node to load and start a controller
+    spawner node to load and activate a controller
 
     Examples # noqa: D416
     --------

controller_manager/controller_manager/unspawner.py
@@ -50,7 +50,7 @@ def main(args=None):
             True,
             True,
             5.0)
-        node.get_logger().info('Stopped controller')
+        node.get_logger().info('Deactivated controller')
 
         ret = unload_controller(node, controller_manager_name, controller_name)
         if not ret.ok:

controller_manager/src/controller_manager.cpp
@@ -639,9 +639,9 @@ controller_interface::return_type ControllerManager::switch_controller(
     {
       RCLCPP_WARN(
         get_logger(),
-        "Could not activate controller with name '%s'. (Check above warnings for more details.) "
+        "Could not activate controller with name '%s'. Check above warnings for more details. "
         "Check the state of the controllers and their required interfaces using "
-        "`ros2 control list_controllers -v` to get more information.",
+        "`ros2 control list_controllers -v` CLI to get more information.",
         (*ctrl_it).c_str());
       if (strictness == controller_manager_msgs::srv::SwitchController::Request::BEST_EFFORT)
       {
@@ -675,7 +675,7 @@ controller_interface::return_type ControllerManager::switch_controller(
     if (!is_controller_active(controller_it->c))
     {
       RCLCPP_WARN(
-        get_logger(), "Controller with name '%s' can not be deactivated since is not active.",
+        get_logger(), "Controller with name '%s' can not be deactivated since it is not active.",
         controller_it->info.name.c_str());
       ret = controller_interface::return_type::ERROR;
     }
@@ -688,9 +688,9 @@ controller_interface::return_type ControllerManager::switch_controller(
     {
       RCLCPP_WARN(
         get_logger(),
-        "Could not deactivate controller with name '%s'. (Check above warnings for more details.)"
+        "Could not deactivate controller with name '%s'. Check above warnings for more details. "
         "Check the state of the controllers and their required interfaces using "
-        "`ros2 control list_controllers -v` to get more information.",
+        "`ros2 control list_controllers -v` CLI to get more information.",
         (*ctrl_it).c_str());
       if (strictness == controller_manager_msgs::srv::SwitchController::Request::BEST_EFFORT)
       {
@@ -1404,9 +1404,46 @@ void ControllerManager::switch_controller_service_cb(
   std::lock_guard<std::mutex> guard(services_lock_);
   RCLCPP_DEBUG(get_logger(), "switching service locked");
 
+  //   response->ok = switch_controller(
+  //     request->activate_controllers, request->deactivate_controllers, request->strictness,
+  //     request->activate_asap, request->timeout) == controller_interface::return_type::OK;
+  // TODO(destogl): remove this after deprecated fields are removed from service and use the
+  // commented three lines above
+  // BEGIN: remove when deprecated removed
+  auto activate_controllers = request->activate_controllers;
+  auto deactivate_controllers = request->deactivate_controllers;
+
+  if (!request->start_controllers.empty())
+  {
+    RCLCPP_WARN(
+      get_logger(),
+      "'start_controllers' field is deprecated, use 'activate_controllers' field instead!");
+    activate_controllers.insert(
+      activate_controllers.end(), request->start_controllers.begin(),
+      request->start_controllers.end());
+  }
+  if (!request->stop_controllers.empty())
+  {
+    RCLCPP_WARN(
+      get_logger(),
+      "'stop_controllers' field is deprecated, use 'deactivate_controllers' field instead!");
+    deactivate_controllers.insert(
+      deactivate_controllers.end(), request->stop_controllers.begin(),
+      request->stop_controllers.end());
+  }
+
+  auto activate_asap = request->activate_asap;
+  if (request->start_asap)
+  {
+    RCLCPP_WARN(
+      get_logger(), "'start_asap' field is deprecated, use 'activate_asap' field instead!");
+    activate_asap = request->start_asap;
+  }
+
   response->ok = switch_controller(
-                   request->start_controllers, request->stop_controllers, request->strictness,
-                   request->start_asap, request->timeout) == controller_interface::return_type::OK;
+                   activate_controllers, deactivate_controllers, request->strictness, activate_asap,
+                   request->timeout) == controller_interface::return_type::OK;
+  // END: remove when deprecated removed
 
   RCLCPP_DEBUG(get_logger(), "switching service finished");
 }

controller_manager_msgs/srv/SwitchController.srv
@@ -1,16 +1,16 @@
-# The SwitchController service allows you stop a number of controllers
-# and start a number of controllers, all in one single timestep of the
-# controller_manager control loop.
+# The SwitchController service allows you deactivate a number of controllers
+# and activate a number of controllers, all in one single timestep of the
+# controller manager's control loop.
 
 # To switch controllers, specify
-#  * the list of controller names to start,
-#  * the list of controller names to stop, and
+#  * the list of controller names to activate,
+#  * the list of controller names to deactivate, and
 #  * the strictness (BEST_EFFORT or STRICT)
 #    * STRICT means that switching will fail if anything goes wrong (an invalid
-#      controller name, a controller that failed to start, etc. )
+#      controller name, a controller that failed to activate, etc. )
 #    * BEST_EFFORT means that even when something goes wrong with on controller,
-#      the service will still try to start/stop the remaining controllers
-#  * start the controllers as soon as their hardware dependencies are ready, will
+#      the service will still try to activate/stop the remaining controllers
+#  * activate the controllers as soon as their hardware dependencies are ready, will
 #    wait for all interfaces to be ready otherwise
 #  * the timeout before aborting pending controllers. Zero for infinite
 
@@ -19,12 +19,15 @@
 # specified strictness.
 
 
-string[] start_controllers
-string[] stop_controllers
+string[] activate_controllers
+string[] deactivate_controllers
+string[] start_controllers       # DEPRECATED: Use activate_controllers filed instead
+string[] stop_controllers        # DEPRECATED: Use deactivate_controllers filed instead
 int32 strictness
 int32 BEST_EFFORT=1
 int32 STRICT=2
-bool start_asap
+bool start_asap                 # DEPRECATED: Use activate_asap filed instead
+bool activate_asap
 builtin_interfaces/Duration timeout
 ---
 bool ok

ros2controlcli/ros2controlcli/verb/load_controller.py
@@ -30,7 +30,7 @@ class LoadControllerVerb(VerbExtension):
         arg.completer = ControllerNameCompleter()
         arg = parser.add_argument(
             '--set-state',
-            choices=['configure', 'start'],
+            choices=['configured', 'active'],
             help='Set the state of the loaded controller',
         )
         add_controller_mgr_parsers(parser)
@@ -50,12 +50,17 @@ class LoadControllerVerb(VerbExtension):
             if not response.ok:
                 return 'Error configuring controller'
 
+            # TODO(destogl): remove in humble+
             if args.set_state == 'start':
+                print('Setting state "start" is deprecated "activate" instead!')
+                args.set_state == 'activate'
+
+            if args.set_state == 'active':
                 response = switch_controllers(
                     node, args.controller_manager, [], [args.controller_name], True, True, 5.0
                 )
                 if not response.ok:
-                    return 'Error starting controller, check controller_manager logs'
+                    return 'Error activating controller, check controller_manager logs'
 
             print(f'Sucessfully loaded controller {args.controller_name} into '
                   f'state { "inactive" if args.set_state == "configure" else "active" }')

ros2controlcli/ros2controlcli/verb/set_controller_state.py
@@ -30,7 +30,8 @@ class SetControllerStateVerb(VerbExtension):
         arg.completer = LoadedControllerNameCompleter()
         arg = parser.add_argument(
             'state',
-            choices=['configure', 'start', 'stop'],
+            # choices=['unconfigured', 'inactive', 'active'], TODO(destogl): when cleanup is impl
+            choices=['inactive', 'active'],
             help='State in which the controller should be changed to',
         )
         add_controller_mgr_parsers(parser)
@@ -44,43 +45,65 @@ class SetControllerStateVerb(VerbExtension):
             except IndexError:
                 return f'controller {args.controller_name} does not seem to be loaded'
 
+            # TODO(destogl): This has to be implemented in CLI and controller manager
+            # if args.state == 'unconfigured':
+            #     if matched_controller.state != 'inactive':
+            #         return f'cannot cleanup {matched_controller.name} ' \
+            #                f'from its current state {matched_controller.state}'
+            #     response = cleanup_controller(
+            #         node, args.controller_manager, args.controller_name
+            #     )
+            #     if not response.ok:
+            #         return 'Error cleaning up controller, check controller_manager logs'
+            #          #      print(f'successfully cleaned up {args.controller_name}')
+            #     return 0
+
             if args.state == 'configure':
-                if matched_controller.state != 'unconfigured':
-                    return f'cannot configure {matched_controller.name} ' \
+                args.state = 'inactive'
+                print('Setting state "configure" is deprecated, use "inactive" instead!')
+
+            if args.state == 'stop':
+                args.state = 'inactive'
+                print('Setting state "stop" is deprecated, use "inactive" instead!')
+
+            if args.state == 'inactive':
+                if matched_controller.state == 'unconfigured':
+                    response = configure_controller(
+                        node, args.controller_manager, args.controller_name
+                    )
+                    if not response.ok:
+                        return 'Error configuring controller, check controller_manager logs'
+
+                    print(f'successfully configured {args.controller_name}')
+                    return 0
+
+                elif matched_controller.state == 'active':
+                    response = switch_controllers(
+                        node, args.controller_manager, [args.controller_name], [], True, True, 5.0
+                    )
+                    if not response.ok:
+                        return 'Error stopping controller, check controller_manager logs'
+
+                    print(f'successfully deactivated {args.controller_name}')
+                    return 0
+
+                else:
+                    return f'cannot put {matched_controller.name} in "inactive" state' \
                            f'from its current state {matched_controller.state}'
 
-                response = configure_controller(
-                    node, args.controller_manager, args.controller_name
-                )
-                if not response.ok:
-                    return 'Error configuring controller, check controller_manager logs'
-
-                print(f'successfully configured {args.controller_name}')
-                return 0
-
             if args.state == 'start':
+                args.state = 'active'
+                print('Setting state "start" is deprecated, use "active" instead!')
+
+            if args.state == 'active':
                 if matched_controller.state != 'inactive':
-                    return f'cannot start {matched_controller.name} ' \
+                    return f'cannot activate {matched_controller.name} ' \
                            f'from its current state {matched_controller.state}'
                 response = switch_controllers(
                     node, args.controller_manager, [], [args.controller_name], True, True, 5.0
                 )
                 if not response.ok:
-                    return 'Error starting controller, check controller_manager logs'
+                    return 'Error activating controller, check controller_manager logs'
 
-                print(f'successfully started {args.controller_name}')
-                return 0
-
-            if args.state == 'stop':
-                if matched_controller.state != 'active':
-                    return f'cannot stop {matched_controller.name} ' \
-                           f'from its current state {matched_controller.state}'
-
-                response = switch_controllers(
-                    node, args.controller_manager, [args.controller_name], [], True, True, 5.0
-                )
-                if not response.ok:
-                    return 'Error stopping controller, check controller_manager logs'
-
-                print(f'successfully stopped {args.controller_name}')
+                print(f'successfully activated {args.controller_name}')
                 return 0

commit 64770522e2f98b5abaa0c1f80385739bd366eccc

spawner.py: Fix python logging on deprecation warning
This was an error introduced in abd5d6e6c8264706b58dddc877063f884cf34a58

controller_manager/controller_manager/spawner.py
@@ -239,7 +239,7 @@ def main(args=None):
                 node.get_logger().info('Deactivated controller')
 
             elif args.stopped:
-                node.get_logger.warn('"--stopped" flag is deprecated use "--inactive" instead')
+                node.get_logger().warn('"--stopped" flag is deprecated use "--inactive" instead')
 
             ret = unload_controller(
                 node, controller_manager_name, controller_name)