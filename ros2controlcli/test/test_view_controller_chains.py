f650aaba5693cecfc4059334371d6ca91a9bc57c
Paul Gesel <paulgesel@gmail.com>
2022-08-21 12:15:51 -0600

    Update ros2controlcli/test/test_view_controller_chains.py
    Co-authored-by: Bence Magyar <bence.magyar.robotics@gmail.com>

--- ros2controlcli/test/test_view_controller_chains.py
+++ ros2controlcli/test/test_view_controller_chains.py
@@ -56,8 +56,6 @@ class TestViewControllerChains(unittest.TestCase):
             chained_to_controller.reference_interfaces.append(f"joint{i}/position")
             chained_to_controller.reference_interfaces.append(f"joint{i}/velocity")
 
-            # chained_from_controller.claimed_interfaces.append(f"{chained_to_controller.name}/joint{i}/position")
-            # chained_from_controller.claimed_interfaces.append(f"{chained_to_controller.name}/joint{i}/velocity")
             chained_from_controller.required_state_interfaces.append(f"joint{i}/position")
             chained_from_controller.required_state_interfaces.append(f"joint{i}/velocity")
 
