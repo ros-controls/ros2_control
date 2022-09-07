94b6f548ae987d11e9126dc45bd670ee86b76b5c
Denis Štogl <denis@stoglrobotics.de>
2022-09-07 19:46:16 +0200

    Remove autodeclare of parameters for controllers. (#757)

--- controller_interface/include/controller_interface/controller_interface_base.hpp
+++ controller_interface/include/controller_interface/controller_interface_base.hpp
@@ -89,10 +89,7 @@ public:
   CONTROLLER_INTERFACE_PUBLIC
   virtual return_type init(
     const std::string & controller_name, const std::string & namespace_ = "",
-    const rclcpp::NodeOptions & node_options =
-      rclcpp::NodeOptions()
-        .allow_undeclared_parameters(true)
-        .automatically_declare_parameters_from_overrides(true));
+    const rclcpp::NodeOptions & node_options = rclcpp::NodeOptions());
 
   /// Custom configure method to read additional parameters for controller-nodes
   /*
