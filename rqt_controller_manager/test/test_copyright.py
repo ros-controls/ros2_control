7a84a4f86236655dfb17a39c30fd7ca3cfbe87a4
Kenji Brameld <kenjibrameld@gmail.com>
2022-09-18 16:04:49 +0900

    don't skip copyright check
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>

--- rqt_controller_manager/test/test_copyright.py
+++ rqt_controller_manager/test/test_copyright.py
@@ -16,8 +16,6 @@ from ament_copyright.main import main
 import pytest
 
 
-# Remove the `skip` decorator once the source file(s) have a copyright header
-@pytest.mark.skip(reason='No copyright header has been placed in the generated source file.')
 @pytest.mark.copyright
 @pytest.mark.linter
 def test_copyright():
