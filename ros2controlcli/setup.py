02e88365a8a67fae55d9016a5394fd66090bbad8
Bence Magyar <bence.magyar.robotics@gmail.com>
2022-09-19 16:36:43 +0100

    3.0.0

--- ros2controlcli/setup.py
+++ ros2controlcli/setup.py
@@ -5,7 +5,7 @@ package_name = 'ros2controlcli'
 
 setup(
     name=package_name,
-    version='2.15.0',
+    version='3.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
