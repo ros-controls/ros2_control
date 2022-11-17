2266b609e80051abf9fd3a212d7ad54199668123
Kenji Brameld <kenjibrameld@gmail.com>
2022-11-18 00:48:18 +0900

    rqt controller manager ros 2 port (#813)
    * add rqt_controller_manager
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * port rqt_controller_manager package.xml from ros1
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * add controller_manager_utils package
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * copy utils.py from ros1
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * override utils.py with one from rqt_joint_trajectory_controlle
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * updates for ros2
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * got controllers loading and unloading
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * fix up state transition
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * don't skip copyright check
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * fill maintainer tag
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * got loading controller names from parameters somewhat working
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * rename get_rosparam_controller_names to get_parameter_controller_names
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * change logic to use node names rather than namespaces
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * allow activation of controller from rqt_controller_manager
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * fix up imports
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * use services from controller_manager_services instead of setting up service clients
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * clean up cm_name
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * clean up,  change "stop" to "deactivate"
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * remove controller_manager_utils, and port necessary functionality into controller_manager.py
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * fix format and copyright to pass tests
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * change license of rqt_controller_manager
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * update plugin.xml
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * remove redundant dependency
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * stretch last section in rqt_controller_manager table
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * update docstring
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * Update rqt_controller_manager/rqt_controller_manager/update_combo.py
    
    Co-authored-by: Erick G. Islas-Osuna <erickisos653@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Erick G. Islas-Osuna <erickisos653@gmail.com>
    
    * Updated to work on rolling.
    
    * Fixup formatting.
    
    * Update maintainers in rqt_controller_manager/package.xml
    
    Co-authored-by: Denis Štogl <denis@stoglrobotics.de>
    
    * update maintainers, and remove ijnek
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    * remove tests, since they're covered in CI
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    Co-authored-by: Erick G. Islas-Osuna <erickisos653@gmail.com>
    Co-authored-by: Denis Štogl <denis@stoglrobotics.de>
    Co-authored-by: Bence Magyar <bence.magyar.robotics@gmail.com>
    (cherry picked from commit de3a01eeda7673a570b38974679732496ff0e0de)

---
+++ rqt_controller_manager/rqt_controller_manager/update_combo.py
@@ -0,0 +1,56 @@
+# Copyright 2021 PAL Robotics S.L.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+def update_combo(combo, new_vals):
+    """
+    Update the contents of a combo box with a set of new values.
+
+    If the previously selected element is still present in the new values, it
+    will remain as active selection, even if its index has changed. This will
+    not trigger any signal.
+
+    If the previously selected element is no longer present in the new values,
+    the combo will unset its selection. This will trigger signals for changed
+    element.
+    """
+    selected_val = combo.currentText()
+    old_vals = [combo.itemText(i) for i in range(combo.count())]
+
+    # Check if combo items changed
+    if not _is_permutation(old_vals, new_vals):
+        # Determine if selected value is in the new list
+        selected_id = -1
+        try:
+            selected_id = new_vals.index(selected_val)
+        except (ValueError):
+            combo.setCurrentIndex(-1)
+
+        # Re-populate items
+        combo.blockSignals(True)  # No need to notify these changes
+        combo.clear()
+        combo.insertItems(0, new_vals)
+        combo.setCurrentIndex(selected_id)  # Restore selection
+        combo.blockSignals(False)
+
+
+def _is_permutation(a, b):
+    """
+    Return true if a is a permutation of b, false otherwise.
+
+    @type a []
+    @type b []
+    @return True if C{a} is a permutation of C{b}, false otherwise
+    @rtype bool
+    """
+    return set(a) == set(b)
