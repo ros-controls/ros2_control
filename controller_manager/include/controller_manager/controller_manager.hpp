737b61acadb9820f7abcf64213e06a23acd928bb
techlabs <hugo.borne-pons@accenture.com>
2022-11-29 11:01:48 +0100

    fix forgotten rebase conflicts

--- controller_manager/include/controller_manager/controller_manager.hpp
+++ controller_manager/include/controller_manager/controller_manager.hpp
@@ -79,14 +79,10 @@ public:
   virtual ~ControllerManager() = default;
 
   CONTROLLER_MANAGER_PUBLIC
-<<<<<<< HEAD
   void init_resource_manager(const std::string & robot_description);
 
   CONTROLLER_MANAGER_PUBLIC
   controller_interface::ControllerInterfaceBaseSharedPtr load_controller(
-=======
-  controller_interface::ControllerInterfaceSharedPtr load_controller(
->>>>>>> remove useless function arguments
     const std::string & controller_name, const std::string & controller_type);
 
   /// load_controller loads a controller by name, the type must be defined in the parameter server.
@@ -96,11 +92,7 @@ public:
    * \see Documentation in controller_manager_msgs/LoadController.srv
    */
   CONTROLLER_MANAGER_PUBLIC
-<<<<<<< HEAD
   controller_interface::ControllerInterfaceBaseSharedPtr load_controller(
-=======
-  controller_interface::ControllerInterfaceSharedPtr load_controller(
->>>>>>> remove useless function arguments
     const std::string & controller_name);
 
   CONTROLLER_MANAGER_PUBLIC
