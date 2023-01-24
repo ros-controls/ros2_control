e534f9df84c98b140f29e6013ffb30a624a341e1
Andy McEvoy <mcevoy.andy@gmail.com>
2023-01-23 20:16:46 -0600

    Update ros2controlcli/setup.py
    Co-authored-by: Bence Magyar <bence.magyar.robotics@gmail.com>

--- ros2controlcli/setup.py
+++ ros2controlcli/setup.py
@@ -1,4 +1,4 @@
-# Copyright 2023 ROS2-Control Development Team
+# Copyright 2023 ros2_control Development Team
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
