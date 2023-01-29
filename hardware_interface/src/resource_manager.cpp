ab2c69b03130e3e9a40f0e723da88e2a98ed29a4
Márk Szitanics <szitanics@gmail.com>
2023-01-30 00:57:00 +0100

    Remove std::terminate() call used for debugging

--- hardware_interface/src/resource_manager.cpp
+++ hardware_interface/src/resource_manager.cpp
@@ -98,7 +98,6 @@ public:
     component_info.type = hardware_info.type;
     component_info.plugin_name = hardware_info.hardware_plugin_name;
     component_info.is_async = hardware_info.is_async;
-    if(component_info.is_async) { std::terminate(); }
 
     hardware_info_map_.insert(std::make_pair(component_info.name, component_info));
     hardware_used_by_controllers_.insert(
