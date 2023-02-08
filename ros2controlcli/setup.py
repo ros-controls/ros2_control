e7bab63b2cd7bd2416ac270f5dab8049afcdd656
Andy McEvoy <mcevoy.andy@gmail.com>
2023-02-08 08:54:17 -0600

    Closes #796 - Adds list_hardware_components to CLI (#891)

--- ros2controlcli/setup.py
+++ ros2controlcli/setup.py
@@ -1,3 +1,17 @@
+# Copyright 2023 ros2_control Development Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from setuptools import find_packages
 from setuptools import setup
 
@@ -37,6 +51,8 @@ ROS2 Control command  interface.""",
         'ros2controlcli.verb': [
             'list_controllers = ros2controlcli.verb.list_controllers:ListControllersVerb',
             'view_controller_chains = ros2controlcli.verb.view_controller_chains:ViewControllerChainsVerb',
+            'list_hardware_components = \
+                ros2controlcli.verb.list_hardware_components:ListHardwareComponentsVerb',
             'list_hardware_interfaces = \
                 ros2controlcli.verb.list_hardware_interfaces:ListHardwareInterfacesVerb',
             'list_controller_types = \
