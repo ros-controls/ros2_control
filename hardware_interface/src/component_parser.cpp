ca26786d253dcddb74689fec9d0616f3d05a2773
Bence Magyar <bence.magyar.robotics@gmail.com>
2023-03-08 18:52:57 +0000

    Revert "Make double parsing locale independent (#921)"
    This reverts commit 78bb0cacb4434006f4e6ed13a94a77bf2261a489.

--- hardware_interface/src/component_parser.cpp
+++ hardware_interface/src/component_parser.cpp
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 #include <tinyxml2.h>
-#include <charconv>
 #include <regex>
 #include <stdexcept>
 #include <string>
@@ -124,30 +123,26 @@ double get_parameter_value_or(
 {
   while (params_it)
   {
-    // Fill the map with parameters
-    const auto tag_name = params_it->Name();
-    if (strcmp(tag_name, parameter_name) == 0)
+    try
     {
-      const auto tag_text = params_it->GetText();
-      if (tag_text)
+      // Fill the map with parameters
+      const auto tag_name = params_it->Name();
+      if (strcmp(tag_name, parameter_name) == 0)
       {
-        // Parse and return double value if there is no parsing error
-        double result_value;
-        const auto parse_result =
-          std::from_chars(tag_text, tag_text + std::strlen(tag_text), result_value);
-        if (parse_result.ec == std::errc())
+        const auto tag_text = params_it->GetText();
+        if (tag_text)
         {
-          return result_value;
+          return std::stod(tag_text);
         }
-
-        // Parsing failed - exit loop and return default value
-        break;
       }
+    }
+    catch (const std::exception & e)
+    {
+      return default_value;
     }
 
     params_it = params_it->NextSiblingElement();
   }
-
   return default_value;
 }
 
