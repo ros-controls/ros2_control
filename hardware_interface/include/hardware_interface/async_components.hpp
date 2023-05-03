7e124366c4e9b9ee026be8aa34ff388fd23dfc05
Bence Magyar <bence.magyar.robotics@gmail.com>
2023-05-03 18:34:36 +0100

    fix copyright

--- hardware_interface/include/hardware_interface/async_components.hpp
+++ hardware_interface/include/hardware_interface/async_components.hpp
@@ -1,4 +1,4 @@
-// Copyright 2023 Open Source Robotics Foundation, Inc.
+// Copyright 2023 ros2_control development team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
