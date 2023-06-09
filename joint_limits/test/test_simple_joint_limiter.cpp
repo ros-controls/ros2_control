c338b8cfefb10d3dd481a8ea78c978ff1b14c580
gwalck <guillaume.walck@stoglrobotics.de>
2023-06-09 10:44:34 +0200

    Updated authorship
    Co-authored-by: Dr. Denis <denis@stoglrobotics.de>

--- joint_limits/test/test_simple_joint_limiter.cpp
+++ joint_limits/test/test_simple_joint_limiter.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/// \author Dr. Denis Stogl
+/// \author Dr. Denis Stogl, Guillaume Walck
 
 #include "test_simple_joint_limiter.hpp"
 
