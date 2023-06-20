61d9648b4080aadb5cb2c6a01a6d8243973d3294
Bence Magyar <bence.magyar.robotics@gmail.com>
2023-06-20 21:02:07 +0100

    fix format

--- ros2controlcli/ros2controlcli/verb/list_hardware_components.py
+++ ros2controlcli/ros2controlcli/verb/list_hardware_components.py
@@ -47,9 +47,11 @@ class ListHardwareComponentsVerb(VerbExtension):
                 else:
                     plugin_name = f"{bcolors.WARNING}plugin name missing!{bcolors.ENDC}"
 
-                print(f"\tplugin name: {plugin_name}\n"
-                      f"\tstate: id={component.state.id} label={component.state.label}\n"
-                      f"\tcommand interfaces")
+                print(
+                    f"\tplugin name: {plugin_name}\n"
+                    f"\tstate: id={component.state.id} label={component.state.label}\n"
+                    f"\tcommand interfaces"
+                )
                 for cmd_interface in component.command_interfaces:
                     if cmd_interface.is_available:
                         available_str = f"{bcolors.OKBLUE}[available]{bcolors.ENDC}"
