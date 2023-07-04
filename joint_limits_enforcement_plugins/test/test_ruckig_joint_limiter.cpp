2a8525f3616adb39e248d880239886200d42c293
Guillaume Walck <guillaume.walck@stoglrobotics.de>
2023-07-04 11:54:41 +0200

    Added and fixed tests
    when current_state differs from configured state
    enforce current_state is required to get
    correct computations

--- joint_limits_enforcement_plugins/test/test_ruckig_joint_limiter.cpp
+++ joint_limits_enforcement_plugins/test/test_ruckig_joint_limiter.cpp
@@ -72,7 +72,65 @@ TEST_F(RuckigJointLimiterTest, when_within_limits_expect_no_limits_applied)
   }
 }
 
-TEST_F(RuckigJointLimiterTest, when_pos_vel_leads_to_acc_exceeded_expect_vel_and_acc_enforced)
+TEST_F(RuckigJointLimiterTest, when_positive_velocity_exceeded_expect_acc_vel_enforced)
+{
+  SetupNode("ruckig_joint_limiter");
+  Load();
+
+  if (joint_limiter_)
+  {
+    Init();
+    Configure();
+
+    rclcpp::Duration period(0, 5000000);  // 0.005 second AS HARDCODED in the lib
+
+    // desired pos exceeds
+    desired_joint_states_.positions[0] = 1.0;
+    desired_joint_states_.velocities[0] = 3.0;
+    ASSERT_TRUE(joint_limiter_->enforce(current_joint_states_, desired_joint_states_, period));
+
+    // check if pos limits applied
+    CHECK_STATE_SINGLE_JOINT(
+      desired_joint_states_, 0,
+      desired_joint_states_.positions[0],  // pos unchanged
+      // TODO(gwalck) why is the result divided by 2.0 ?
+      desired_joint_states_.accelerations[0] * period.seconds() /
+        2.0,                    // vel limited to max_acc * dt / 2.0
+      100.0 * period.seconds()  // acc set to max_jerk * dt
+    );
+  }
+}
+
+TEST_F(RuckigJointLimiterTest, when_negative_velocity_exceeded_expect_acc_vel_enforced)
+{
+  SetupNode("ruckig_joint_limiter");
+  Load();
+
+  if (joint_limiter_)
+  {
+    Init();
+    Configure();
+
+    rclcpp::Duration period(0, 5000000);  // 0.005 second AS HARDCODED in the lib
+
+    // desired pos exceeds
+    desired_joint_states_.positions[0] = -1.0;
+    desired_joint_states_.velocities[0] = -3.0;
+    ASSERT_TRUE(joint_limiter_->enforce(current_joint_states_, desired_joint_states_, period));
+
+    // check if pos limits applied
+    CHECK_STATE_SINGLE_JOINT(
+      desired_joint_states_, 0,
+      desired_joint_states_.positions[0],  // pos unchanged
+      // TODO(gwalck) why is the result divided by 2.0 ?
+      desired_joint_states_.accelerations[0] * period.seconds() /
+        2.0,                     // vel limited to max_acc * dt / 2.0
+      -100.0 * period.seconds()  // acc set to max_jerk * dt
+    );
+  }
+}
+
+TEST_F(RuckigJointLimiterTest, when_incr_vel_leads_to_acc_exceeded_expect_vel_and_acc_enforced)
 {
   SetupNode("ruckig_joint_limiter");
   Load();
@@ -101,7 +159,7 @@ TEST_F(RuckigJointLimiterTest, when_pos_vel_leads_to_acc_exceeded_expect_vel_and
   }
 }
 
-TEST_F(RuckigJointLimiterTest, when_neg_vel_leads_to_acc_exceeded_expect_vel_and_acc_enforced)
+TEST_F(RuckigJointLimiterTest, when_decr_vel_leads_to_acc_exceeded_expect_vel_and_acc_enforced)
 {
   SetupNode("ruckig_joint_limiter");
   Load();
@@ -157,7 +215,63 @@ TEST_F(RuckigJointLimiterTest, when_position_exceeded_expect_nochange)
   }
 }
 
-TEST_F(RuckigJointLimiterTest, when_acceleration_exceeded_expect_acc_enforced)
+TEST_F(RuckigJointLimiterTest, when_acc_within_jerk_limit_exceeded_expect_vel_and_acc_enforced)
+{
+  SetupNode("ruckig_joint_limiter");
+  Load();
+
+  if (joint_limiter_)
+  {
+    Init();
+    Configure();
+
+    rclcpp::Duration period(0, 5000000);  // 0.005 second AS HARDCODED in the lib
+
+    // desired acc exceed but within jerk limits
+    current_joint_states_.velocities[0] = 1.0;
+    current_joint_states_.accelerations[0] = 4.6;
+    // required to initialize the limiter correctly
+    joint_limiter_->configure(current_joint_states_);
+    desired_joint_states_.positions[0] = 1.0;
+    desired_joint_states_.velocities[0] = 1.023;
+    desired_joint_states_.accelerations[0] = 5.1;
+    ASSERT_TRUE(joint_limiter_->enforce(current_joint_states_, desired_joint_states_, period));
+
+    // acc should be close to 5.0
+    ASSERT_GT(desired_joint_states_.accelerations[0], 4.6);
+    ASSERT_LE(desired_joint_states_.accelerations[0], 5.0);
+  }
+}
+
+TEST_F(RuckigJointLimiterTest, when_dec_within_jerk_limit_exceeded_expect_vel_and_acc_enforced)
+{
+  SetupNode("ruckig_joint_limiter");
+  Load();
+
+  if (joint_limiter_)
+  {
+    Init();
+    Configure();
+
+    rclcpp::Duration period(0, 5000000);  // 0.005 second AS HARDCODED in the lib
+
+    // desired acc exceed but within jerk limits
+    current_joint_states_.velocities[0] = -1.0;
+    current_joint_states_.accelerations[0] = -4.6;
+    // required to initialize the limiter correctly
+    joint_limiter_->configure(current_joint_states_);
+    desired_joint_states_.positions[0] = -1.0;
+    desired_joint_states_.velocities[0] = -1.023;
+    desired_joint_states_.accelerations[0] = -5.1;
+    ASSERT_TRUE(joint_limiter_->enforce(current_joint_states_, desired_joint_states_, period));
+
+    // acc should be close to -5.0
+    ASSERT_LT(desired_joint_states_.accelerations[0], -4.6);
+    ASSERT_GE(desired_joint_states_.accelerations[0], -5.0);
+  }
+}
+
+TEST_F(RuckigJointLimiterTest, when_acc_leads_to_jerk_exceeded_expect_acc_enforced)
 {
   SetupNode("ruckig_joint_limiter");
   Load();
@@ -170,7 +284,7 @@ TEST_F(RuckigJointLimiterTest, when_acceleration_exceeded_expect_acc_enforced)
     rclcpp::Duration period(0, 5000000);  // 0.005 second
 
     // desired acceleration exceeds
-    desired_joint_states_.velocities[0] = 0.002; // if no velocity set, acc is computed negative !
+    desired_joint_states_.velocities[0] = 0.002;   // if no velocity set, acc is computed negative !
     desired_joint_states_.accelerations[0] = 0.6;  // leads to jerk > max_jerk
     ASSERT_TRUE(joint_limiter_->enforce(current_joint_states_, desired_joint_states_, period));
 
@@ -186,7 +300,7 @@ TEST_F(RuckigJointLimiterTest, when_acceleration_exceeded_expect_acc_enforced)
   }
 }
 
-TEST_F(RuckigJointLimiterTest, when_deceleration_exceeded_expect_dec_enforced)
+TEST_F(RuckigJointLimiterTest, when_dec_leads_to_jerk_exceeded_expect_dec_enforced)
 {
   SetupNode("ruckig_joint_limiter");
   Load();
@@ -199,7 +313,7 @@ TEST_F(RuckigJointLimiterTest, when_deceleration_exceeded_expect_dec_enforced)
     rclcpp::Duration period(0, 5000000);  // 0.005 second
 
     // desired deceleration exceeds
-    desired_joint_states_.velocities[0] = -0.002; // if no velocity set, acc is computed positve !
+    desired_joint_states_.velocities[0] = -0.002;  // if no velocity set, acc is computed positive !
     desired_joint_states_.accelerations[0] = -0.6;  // leads to jerk > max_jerk
     ASSERT_TRUE(joint_limiter_->enforce(current_joint_states_, desired_joint_states_, period));
 
@@ -212,6 +326,49 @@ TEST_F(RuckigJointLimiterTest, when_deceleration_exceeded_expect_dec_enforced)
         2.0,                     // vel limited to max_acc * dt / 2.0
       -100.0 * period.seconds()  // acc is max_jerk * dt
     );
+  }
+}
+
+TEST_F(RuckigJointLimiterTest, when_vel_close_to_vel_limit_expect_deceleration_enforced)
+{
+  SetupNode("ruckig_joint_limiter");
+  Load();
+
+  if (joint_limiter_)
+  {
+    Init();
+    Configure();
+
+    // using 0.05 because 1.0 sec invalidates the "small dt integration"
+    rclcpp::Duration period(0, 5000000);  // 0.005 second
+
+    // close to vel limit should trigger braking
+    current_joint_states_.positions[0] = 1.0;
+    current_joint_states_.velocities[0] = 1.9;
+    // required to initialize the limiter correctly
+    joint_limiter_->configure(current_joint_states_);
+    desired_joint_states_.positions[0] = 1.5;
+    desired_joint_states_.velocities[0] = 1.98;
+    desired_joint_states_.accelerations[0] = 5.0;
+
+    for (auto i = 0u; i < 4; ++i)
+    {
+      auto previous_vel_request = desired_joint_states_.velocities[0];
+      ASSERT_TRUE(joint_limiter_->enforce(current_joint_states_, desired_joint_states_, period));
+
+      ASSERT_LE(
+        desired_joint_states_.velocities[0],
+        previous_vel_request);  // vel adapted to stay below or equal to previous vel
+
+      ASSERT_LE(desired_joint_states_.velocities[0],
+                2.0);  // vel adapted to stay below max vel
+
+      ASSERT_LE(desired_joint_states_.accelerations[0], 0.0);  // should decelerate
+
+      Integrate(period.seconds());
+
+      ASSERT_LT(current_joint_states_.positions[0], 5.0);  // below joint limit
+    }
   }
 }
 
