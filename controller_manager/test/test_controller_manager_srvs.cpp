a35911567656811fefb81c1d80f8ed59dfb8ab52
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2023-08-28 23:19:13 +0200

    update list_chained_controllers_srv test with more assertions

--- controller_manager/test/test_controller_manager_srvs.cpp
+++ controller_manager/test/test_controller_manager_srvs.cpp
@@ -269,7 +269,9 @@ TEST_F(TestControllerManagerSrvs, list_chained_controllers_srv)
   ASSERT_EQ(result->controller[0].is_chainable, true);
   ASSERT_EQ(result->controller[0].is_chained, false);
   ASSERT_EQ(result->controller[0].reference_interfaces.size(), 2u);
-  ;
+  ASSERT_EQ("joint1/position", result->controller[0].reference_interfaces[0]);
+  ASSERT_EQ("joint1/velocity", result->controller[0].reference_interfaces[1]);
+
   ASSERT_EQ(result->controller[0].chain_connections.size(), 0u);
   // check test controller
   ASSERT_EQ(result->controller[1].state, "inactive");
@@ -300,6 +302,7 @@ TEST_F(TestControllerManagerSrvs, list_chained_controllers_srv)
     test_chainable_controller::TEST_CONTROLLER_NAME,
     result->controller[0].chain_connections[0].name);
   ASSERT_EQ(2u, result->controller[0].chain_connections[0].reference_interfaces.size());
+  ASSERT_EQ("test_chainable_controller_name", result->controller[0].chain_connections[0].name);
   ASSERT_EQ("joint1/position", result->controller[0].chain_connections[0].reference_interfaces[0]);
   ASSERT_EQ("joint1/velocity", result->controller[0].chain_connections[0].reference_interfaces[1]);
 }
