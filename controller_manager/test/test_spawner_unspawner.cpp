91d5f21067de519654f778114d721850e9223db0
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2023-10-22 20:02:02 +0200

    added test for --activate-as-group option

--- controller_manager/test/test_spawner_unspawner.cpp
+++ controller_manager/test/test_spawner_unspawner.cpp
@@ -232,6 +232,15 @@ TEST_F(TestLoadController, multi_ctrls_test_type_in_param)
   {
     validate_loaded_controllers();
   }
+
+  // Now unload everything and load them as a group in a single operation
+  EXPECT_EQ(call_unspawner("ctrl_1 ctrl_2 ctrl_3 -c test_controller_manager"), 0);
+  ASSERT_EQ(cm_->get_loaded_controllers().size(), 0ul) << "Controller should have been unloaded";
+  EXPECT_EQ(call_spawner("ctrl_1 ctrl_2 ctrl_3 -c test_controller_manager --activate-as-group"), 0);
+  ASSERT_EQ(cm_->get_loaded_controllers().size(), 3ul) << "Controller should have been loaded";
+  {
+    validate_loaded_controllers();
+  }
 }
 
 TEST_F(TestLoadController, spawner_test_type_in_arg)
