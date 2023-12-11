f5cedf478cd11aecc28d390502fc9758ce206ed9
Bence Magyar <bence.magyar.robotics@gmail.com>
2023-12-11 18:18:53 +0000

    Reformat with braces added (#1209)
    (cherry picked from commit 562f36ddb615a70b54c71365393a8dc69508f52f)
    
    # Conflicts:
    #	controller_manager/src/controller_manager.cpp

--- controller_manager/test/test_controller_manager_srvs.cpp
+++ controller_manager/test/test_controller_manager_srvs.cpp
@@ -597,7 +597,9 @@ TEST_F(TestControllerManagerSrvs, list_sorted_chained_controllers)
        {TEST_CHAINED_CONTROLLER_4, TEST_CHAINED_CONTROLLER_3, TEST_CHAINED_CONTROLLER_5,
         TEST_CHAINED_CONTROLLER_2, TEST_CHAINED_CONTROLLER_1,
         test_controller::TEST_CONTROLLER_NAME})
+  {
     cm_->configure_controller(controller);
+  }
 
   // get controller list after configure
   result = call_service_and_wait(*client, request, srv_executor);
@@ -754,7 +756,9 @@ TEST_F(TestControllerManagerSrvs, list_sorted_complex_chained_controllers)
        {TEST_CHAINED_CONTROLLER_4, TEST_CHAINED_CONTROLLER_3, TEST_CHAINED_CONTROLLER_5,
         TEST_CHAINED_CONTROLLER_7, TEST_CHAINED_CONTROLLER_2, TEST_CHAINED_CONTROLLER_1,
         TEST_CHAINED_CONTROLLER_6, test_controller::TEST_CONTROLLER_NAME})
+  {
     cm_->configure_controller(controller);
+  }
 
   // get controller list after configure
   result = call_service_and_wait(*client, request, srv_executor);
@@ -968,7 +972,10 @@ TEST_F(TestControllerManagerSrvs, list_sorted_independent_chained_controllers)
                       TEST_CHAINED_CONTROLLER_4, TEST_CONTROLLER_2,
                       TEST_CHAINED_CONTROLLER_2, TEST_CHAINED_CONTROLLER_6,
                       TEST_CHAINED_CONTROLLER_7, TEST_CHAINED_CONTROLLER_8};
-  for (const auto & controller : ctrls_order) cm_->configure_controller(controller);
+  for (const auto & controller : ctrls_order)
+  {
+    cm_->configure_controller(controller);
+  }
 
   // get controller list after configure
   result = call_service_and_wait(*client, request, srv_executor);
