9bb56bbd58d4791bd43fc5514149c712c51540a3
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2023-12-17 16:36:27 +0100

    remove resetting the list on configure as there might be another controller added as predecessor

--- controller_manager/src/controller_manager.cpp
+++ controller_manager/src/controller_manager.cpp
@@ -848,7 +848,6 @@ controller_interface::return_type ControllerManager::configure_controller(
   // let's update the list of following and preceding controllers
   const auto cmd_itfs = controller->command_interface_configuration().names;
   const auto state_itfs = controller->state_interface_configuration().names;
-  controller_chain_spec_[controller_name] = ControllerChainSpec();
   for (const auto & cmd_itf : cmd_itfs)
   {
     controller_manager::ControllersListIterator ctrl_it;
