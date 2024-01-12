dfe034bea19a60843816e771754b6ec39f02a23a
Dr. Denis <denis@stoglrobotics.de>
2024-01-12 15:34:53 +0100

    Fix return of ERROR and calls of cleanup when system is unconfigured of finalized (#1279)
    ---------
    
    Co-authored-by: Andrea Scipione <andrea.scipione@gmail.com>
    Co-authored-by: Bence Magyar <bence.magyar.robotics@gmail.com>
    Co-authored-by: Dr. Denis <denis@stoglrobotics.de>
    (cherry picked from commit acbeeea057aaa07fd9eec4399cc27f912653537a)

--- hardware_interface/src/sensor.cpp
+++ hardware_interface/src/sensor.cpp
@@ -195,7 +195,12 @@ const rclcpp_lifecycle::State & Sensor::get_state() const { return impl_->get_st
 
 return_type Sensor::read(const rclcpp::Time & time, const rclcpp::Duration & period)
 {
-  // TODO(destogl): discuss what should be default return value, e.g., "NOT_EXECUTED"
+  if (
+    impl_->get_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_UNCONFIGURED ||
+    impl_->get_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_FINALIZED)
+  {
+    return return_type::OK;
+  }
   return_type result = return_type::ERROR;
   if (
     impl_->get_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_INACTIVE ||
