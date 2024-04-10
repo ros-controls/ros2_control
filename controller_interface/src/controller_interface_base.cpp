cd62f7884277965cb796c355f46c2057ad30dcd3
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2024-04-10 18:11:47 +0200

    added async_update_ready_ to use with conditional variable to avoid spurious wakeups

--- controller_interface/src/controller_interface_base.cpp
+++ controller_interface/src/controller_interface_base.cpp
@@ -130,17 +130,22 @@ return_type ControllerInterfaceBase::trigger_update(
         while (get_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_ACTIVE &&
                !async_update_stop_)
         {
-          async_update_condition_.wait(lock);
-          lock.unlock();
+          async_update_condition_.wait(lock, []{ return async_update_ready_;});
           async_update_return_ = update(time, period);
+          async_update_ready_ = false;
+          lock.unlock();
         }
       };
       thread_ = std::thread(update_fn);
     }
     else
     {
-      std::lock_guard lk(async_mtx_);
-      async_update_condition_.notify_one();
+      if(!async_update_ready_)
+      {
+        std::lock_guard lk(async_mtx_);
+        async_update_condition_.notify_one();
+        async_update_ready_ = true;
+      }
     }
     return async_update_return_;
   }
