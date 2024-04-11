f575440c0fcc320cd381d72f1e12fb4a2107f946
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2024-04-11 15:19:29 +0200

    add current update time and period to be able to update them

--- controller_interface/include/controller_interface/controller_interface_base.hpp
+++ controller_interface/include/controller_interface/controller_interface_base.hpp
@@ -297,6 +297,8 @@ protected:
 
 private:
   std::shared_ptr<rclcpp_lifecycle::LifecycleNode> node_;
+  rclcpp::Time current_update_time_;
+  rclcpp::Duration current_update_period_;
 
   // Async related variables
   std::thread thread_;
