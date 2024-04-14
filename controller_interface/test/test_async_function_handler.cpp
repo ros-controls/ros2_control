3cd856fb07ee3d5b0128bbfd628f7c1fed6355ca
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2024-04-14 11:22:17 +0200

    Added tests for the new async function handler

---
+++ controller_interface/test/test_async_function_handler.cpp
@@ -0,0 +1,82 @@
+// Copyright 2024 PAL Robotics S.L.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "test_async_function_handler.hpp"
+#include "gmock/gmock.h"
+
+namespace controller_interface
+{
+TestAsyncFunctionHandler::TestAsyncFunctionHandler()
+: state_(rclcpp_lifecycle::State(lifecycle_msgs::msg::State::PRIMARY_STATE_ACTIVE, "test_async")),
+  counter_(0),
+  return_state_(return_type::OK)
+{
+}
+
+TestAsyncFunctionHandler::~TestAsyncFunctionHandler() {}
+
+void TestAsyncFunctionHandler::initialize()
+{
+  handler_.init(
+    std::bind(&TestAsyncFunctionHandler::get_state, this),
+    std::bind(
+      &TestAsyncFunctionHandler::update, this, std::placeholders::_1, std::placeholders::_2));
+}
+
+return_type TestAsyncFunctionHandler::update(
+  const rclcpp::Time & time, const rclcpp::Duration & period)
+{
+  last_callback_time_ = time;
+  last_callback_period_ = period;
+  // to simulate some work being done
+  std::this_thread::sleep_for(std::chrono::microseconds(10));
+  counter_++;
+  return return_state_;
+}
+const rclcpp_lifecycle::State & TestAsyncFunctionHandler::get_state() const { return state_; }
+
+}  // namespace controller_interface
+class AsyncFunctionHandlerTest : public ::testing::Test
+{
+public:
+  static void SetUpTestCase() { rclcpp::init(0, nullptr); }
+
+  static void TearDownTestCase() { rclcpp::shutdown(); }
+};
+
+TEST_F(AsyncFunctionHandlerTest, check_initialization)
+{
+  controller_interface::TestAsyncFunctionHandler async_class;
+
+  ASSERT_FALSE(async_class.get_handler().is_initialized());
+  ASSERT_FALSE(async_class.get_handler().is_async());
+  ASSERT_FALSE(async_class.get_handler().is_running());
+
+  async_class.initialize();
+  ASSERT_TRUE(async_class.get_handler().is_initialized());
+  ASSERT_FALSE(async_class.get_handler().is_async());
+  ASSERT_FALSE(async_class.get_handler().is_running());
+
+  EXPECT_EQ(async_class.get_state().id(), lifecycle_msgs::msg::State::PRIMARY_STATE_ACTIVE);
+  ASSERT_EQ(controller_interface::return_type::OK, async_class.trigger());
+  ASSERT_TRUE(async_class.get_handler().is_initialized());
+  ASSERT_TRUE(async_class.get_handler().is_async());
+  ASSERT_TRUE(async_class.get_handler().is_running());
+  async_class.get_handler().wait_for_update_to_finish();
+  async_class.get_handler().preempt_async_update();
+  ASSERT_EQ(async_class.get_counter(), 1);
+  ASSERT_FALSE(async_class.get_handler().is_async());
+  ASSERT_FALSE(async_class.get_handler().is_running());
+  async_class.get_handler().wait_for_update_to_finish();
+}
---
+++ controller_interface/test/test_async_function_handler.hpp
@@ -0,0 +1,52 @@
+// Copyright 2024 PAL Robotics S.L.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TEST_ASYNC_FUNCTION_HANDLER_HPP_
+#define TEST_ASYNC_FUNCTION_HANDLER_HPP_
+
+#include "controller_interface/async_function_handler.hpp"
+#include "controller_interface/controller_interface_base.hpp"
+
+namespace controller_interface
+{
+class TestAsyncFunctionHandler
+{
+public:
+  TestAsyncFunctionHandler();
+  ~TestAsyncFunctionHandler();
+
+  void initialize();
+
+  ros2_control::AsyncFunctionHandler<return_type> & get_handler() { return handler_; }
+
+  return_type trigger()
+  {
+    return handler_.trigger_async_update(last_callback_time_, last_callback_period_);
+  }
+  return_type update(const rclcpp::Time & time, const rclcpp::Duration & period);
+
+  const rclcpp_lifecycle::State & get_state() const;
+
+  int get_counter() const { return counter_; }
+
+private:
+  rclcpp_lifecycle::State state_;
+  rclcpp::Time last_callback_time_;
+  rclcpp::Duration last_callback_period_{0, 0};
+  int counter_;
+  return_type return_state_;
+  ros2_control::AsyncFunctionHandler<return_type> handler_;
+};
+}  // namespace controller_interface
+#endif  // TEST_ASYNC_FUNCTION_HANDLER_HPP_
