0a99d3ab14d788a58730c6e96b20ee40060800d1
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2024-04-17 18:59:19 +0200

    Add first version of tests into the joint range limiter

---
+++ joint_limits/test/test_joint_range_limiter.hpp
@@ -0,0 +1,116 @@
+// Copyright (c) 2023, Stogl Robotics Consulting UG (haftungsbeschränkt)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TEST_JOINT_SATURATION_LIMITER_HPP_
+#define TEST_JOINT_SATURATION_LIMITER_HPP_
+
+#include <gtest/gtest.h>
+
+#include <memory>
+#include <string>
+#include <vector>
+#include "joint_limits/joint_limiter_interface.hpp"
+#include "joint_limits/joint_limits.hpp"
+#include "pluginlib/class_loader.hpp"
+#include "rclcpp/duration.hpp"
+#include "rclcpp/node.hpp"
+#include "joint_limits/joint_limiter_struct.hpp"
+
+const double COMMON_THRESHOLD = 0.0011;
+
+using JointLimiter = joint_limits::JointLimiterInterface<
+  joint_limits::JointLimits, joint_limits::JointControlInterfacesData>;
+
+class JointSaturationLimiterTest : public ::testing::Test
+{
+public:
+  void SetUp() override
+  {
+    node_name_ = ::testing::UnitTest::GetInstance()->current_test_info()->name();
+  }
+
+  void SetupNode(const std::string node_name = "")
+  {
+    if (!node_name.empty())
+    {
+      node_name_ = node_name;
+    }
+    node_ = std::make_shared<rclcpp::Node>(node_name_);
+  }
+
+  void Load()
+  {
+    joint_limiter_ = std::unique_ptr<JointLimiter>(
+      joint_limiter_loader_.createUnmanagedInstance(joint_limiter_type_));
+  }
+
+  void Init(const std::string & joint_name = "foo_joint")
+  {
+    joint_names_ = {joint_name};
+    joint_limiter_->init(joint_names_, node_);
+    num_joints_ = joint_names_.size();
+    last_commanded_state_.joint_name = joint_name;
+    last_commanded_state_.position = 0.0;
+    last_commanded_state_.velocity= 0.0;
+    last_commanded_state_.acceleration= 0.0;
+    last_commanded_state_.effort= 0.0;
+    desired_state_ = last_commanded_state_;
+    actual_state_ = last_commanded_state_;
+  }
+  
+  void Init(const joint_limits::JointControlInterfacesData & init_state)
+  {
+    last_commanded_state_ = init_state;
+    joint_names_ = {last_commanded_state_.joint_name};
+    joint_limiter_->init(joint_names_, node_);
+    num_joints_ = joint_names_.size();
+    desired_state_ = last_commanded_state_;
+    actual_state_ = last_commanded_state_;
+  }
+
+  void Configure() { joint_limiter_->configure(last_commanded_state_); }
+
+  void Integrate(double dt)
+  {
+    actual_state_.position = actual_state_.position.value() + desired_state_.velocity.value() * dt +
+                                          0.5 * desired_state_.acceleration.value() * dt * dt;
+    actual_state_.velocity = actual_state_.velocity.value() + desired_state_.acceleration.value() * dt;
+  }
+
+  JointSaturationLimiterTest()
+  : joint_limiter_type_("joint_limits/JointInterfacesSaturationLimiter"),
+    joint_limiter_loader_(
+      "joint_limits",
+      "joint_limits::JointLimiterInterface<joint_limits::JointLimits, "
+      "joint_limits::JointControlInterfacesData>")
+  {
+  }
+
+  void TearDown() override { node_.reset(); }
+
+protected:
+  std::string node_name_;
+  rclcpp::Node::SharedPtr node_;
+  std::vector<std::string> joint_names_;
+  size_t num_joints_;
+  std::unique_ptr<JointLimiter> joint_limiter_;
+  std::string joint_limiter_type_;
+  pluginlib::ClassLoader<JointLimiter> joint_limiter_loader_;
+
+  joint_limits::JointControlInterfacesData last_commanded_state_;
+  joint_limits::JointControlInterfacesData desired_state_;
+  joint_limits::JointControlInterfacesData actual_state_;
+};
+
+#endif  // TEST_JOINT_SATURATION_LIMITER_HPP_
