913c9f9c0d5e922945c59881d837d87d4ca5028f
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2024-04-18 15:41:22 +0200

    add minor information

--- controller_interface/include/controller_interface/async_function_handler.hpp
+++ controller_interface/include/controller_interface/async_function_handler.hpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/// \author Sai Kishor Kothakota
+
 #ifndef CONTROLLER_INTERFACE__ASYNC_FUNCTION_HANDLER_HPP_
 #define CONTROLLER_INTERFACE__ASYNC_FUNCTION_HANDLER_HPP_
 
