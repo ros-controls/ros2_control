147519e63344a3b63e78513cc2e6219529499663
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2024-04-21 17:06:26 +0000

    Adaptions for rosdoc2

---
+++ transmission_interface/doc/conf.py
@@ -0,0 +1,5 @@
+# Configuration file for the Sphinx documentation builder.
+# settings will be overridden by rosdoc2, so we add here only custom settings
+
+copyright = "2024, ros2_control development team"
+html_logo = "https://control.ros.org/master/_static/logo_ros-controls.png"
