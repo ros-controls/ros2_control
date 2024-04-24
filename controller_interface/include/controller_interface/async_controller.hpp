9469f8b763352392d784feac548275de63402042
Dr. Denis <denis@stoglrobotics.de>
2024-04-24 18:28:36 +0200

    Update the year in license.

--- controller_interface/include/controller_interface/async_controller.hpp
+++ controller_interface/include/controller_interface/async_controller.hpp
@@ -1,4 +1,4 @@
-// Copyright 2023 ros2_control development team
+// Copyright 2024 ros2_control development team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
