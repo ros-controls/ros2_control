3192bc66e927cea4606fd677e9ca1ffe8a29efbb
Adria Roig <adria.roig@pal-robotics.com>
2024-04-25 10:48:56 +0200

    Move joint limits computation in a separate library

---
+++ joint_limits/include/joint_limits/joint_limits_helpers.hpp
@@ -0,0 +1,48 @@
+// Copyright 2024 PAL Robotics S.L.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/// \author Adrià Roig Moreno
+
+#ifndef JOINT_LIMITS__JOINT_LIMITS_HELPERS_HPP_
+#define JOINT_LIMITS__JOINT_LIMITS_HELPERS_HPP_
+
+#include "joint_limits/joint_limits.hpp"
+#include <optional>
+
+namespace joint_limits
+{
+
+constexpr size_t ROS_LOG_THROTTLE_PERIOD = 1 * 1000;  // Milliseconds to throttle logs inside loops
+constexpr double VALUE_CONSIDERED_ZERO = 1e-10;
+
+bool is_limited(double value, double min, double max);
+
+std::pair<double, double> compute_position_limits(
+  const joint_limits::JointLimits & limits, const std::optional<double> & act_vel,
+  const std::optional<double> & prev_command_pos, double dt);
+
+std::pair<double, double> compute_velocity_limits(
+  const std::string & joint_name, const joint_limits::JointLimits & limits,
+  const std::optional<double> & act_pos, const std::optional<double> & prev_command_vel, double dt);
+
+std::pair<double, double> compute_effort_limits(
+  const joint_limits::JointLimits & limits, const std::optional<double> & act_pos,
+  const std::optional<double> & act_vel, double /*dt*/);
+
+std::pair<double, double> compute_acceleration_limits(
+    const JointLimits &limits, double desired_acceleration, std::optional<double> actual_velocity);
+
+} // namespace joint_limits
+
+#endif // JOINT_LIMITS__JOINT_LIMITS_HELPERS_HPP_
--- joint_limits/src/joint_range_limiter.cpp
+++ joint_limits/src/joint_range_limiter.cpp
@@ -20,119 +20,7 @@
 #include "joint_limits/joint_limiter_struct.hpp"
 #include "rclcpp/duration.hpp"
 #include "rcutils/logging_macros.h"
-
-bool is_limited(double value, double min, double max) { return value < min || value > max; }
-
-std::pair<double, double> compute_position_limits(
-  const joint_limits::JointLimits & limits, const std::optional<double> & act_vel,
-  const std::optional<double> & prev_command_pos, double dt)
-{
-  std::pair<double, double> pos_limits({limits.min_position, limits.max_position});
-  if (limits.has_velocity_limits)
-  {
-    const double act_vel_abs = act_vel.has_value() ? std::fabs(act_vel.value()) : 0.0;
-    const double delta_vel = limits.has_acceleration_limits
-                               ? act_vel_abs + (limits.max_acceleration * dt)
-                               : limits.max_velocity;
-    const double max_vel = std::min(limits.max_velocity, delta_vel);
-    const double delta_pos = max_vel * dt;
-    pos_limits.first = std::max(prev_command_pos.value() - delta_pos, pos_limits.first);
-    pos_limits.second = std::min(prev_command_pos.value() + delta_pos, pos_limits.second);
-  }
-  return pos_limits;
-}
-
-std::pair<double, double> compute_position_limits(
-  const joint_limits::JointLimits & limits, const joint_limits::SoftJointLimits & soft_limits,
-  double prev_command_pos, double dt)
-{
-  std::pair<double, double> pos_limits({limits.min_position, limits.max_position});
-
-  // velocity bounds
-  double soft_min_vel = -limits.max_velocity;
-  double soft_max_vel = limits.max_velocity;
-
-  if (limits.has_position_limits)
-  {
-    soft_min_vel = std::clamp(
-      -soft_limits.k_position * (prev_command_pos - soft_limits.min_position), -limits.max_velocity,
-      limits.max_velocity);
-    soft_max_vel = std::clamp(
-      -soft_limits.k_position * (prev_command_pos - soft_limits.max_position), -limits.max_velocity,
-      limits.max_velocity);
-  }
-  // Position bounds
-  pos_limits.first = prev_command_pos + soft_min_vel * dt;
-  pos_limits.second = prev_command_pos + soft_max_vel * dt;
-  return pos_limits;
-}
-
-std::pair<double, double> compute_velocity_limits(
-  const std::string & joint_name, const joint_limits::JointLimits & limits,
-  const std::optional<double> & act_pos, const std::optional<double> & prev_command_vel, double dt)
-{
-  const double max_vel =
-    limits.has_velocity_limits ? limits.max_velocity : std::numeric_limits<double>::infinity();
-  std::pair<double, double> vel_limits({-max_vel, max_vel});
-  if (limits.has_position_limits && act_pos.has_value())
-  {
-    const double max_vel_with_pos_limits = (limits.max_position - act_pos.value()) / dt;
-    const double min_vel_with_pos_limits = (limits.min_position - act_pos.value()) / dt;
-    vel_limits.first = std::max(min_vel_with_pos_limits, vel_limits.first);
-    vel_limits.second = std::min(max_vel_with_pos_limits, vel_limits.second);
-    if (act_pos.value() > limits.max_position || act_pos.value() < limits.min_position)
-    {
-      RCLCPP_ERROR_ONCE(
-        rclcpp::get_logger("joint_limiter_interface"),
-        "Joint position is out of bounds for the joint : '%s'. Joint velocity limits will be "
-        "restrictred to zero.",
-        joint_name.c_str());
-      vel_limits = {0.0, 0.0};
-    }
-  }
-  if (limits.has_acceleration_limits && prev_command_vel.has_value())
-  {
-    const double delta_vel = limits.max_acceleration * dt;
-    vel_limits.first = std::max(prev_command_vel.value() - delta_vel, vel_limits.first);
-    vel_limits.second = std::min(prev_command_vel.value() + delta_vel, vel_limits.second);
-  }
-  return vel_limits;
-}
-
-std::pair<double, double> compute_effort_limits(
-  const joint_limits::JointLimits & limits, const std::optional<double> & act_pos,
-  const std::optional<double> & act_vel, double /*dt*/)
-{
-  const double max_effort =
-    limits.has_effort_limits ? limits.max_effort : std::numeric_limits<double>::infinity();
-  std::pair<double, double> eff_limits({-max_effort, max_effort});
-  if (limits.has_position_limits && act_pos.has_value() && act_vel.has_value())
-  {
-    if ((act_pos.value() <= limits.min_position) && (act_vel.value() <= 0.0))
-    {
-      eff_limits.first = 0.0;
-    }
-    else if ((act_pos.value() >= limits.max_position) && (act_vel.value() >= 0.0))
-    {
-      eff_limits.second = 0.0;
-    }
-  }
-  if (limits.has_velocity_limits && act_vel.has_value())
-  {
-    if (act_vel.value() < -limits.max_velocity)
-    {
-      eff_limits.first = 0.0;
-    }
-    else if (act_vel.value() > limits.max_velocity)
-    {
-      eff_limits.second = 0.0;
-    }
-  }
-  return eff_limits;
-}
-
-constexpr size_t ROS_LOG_THROTTLE_PERIOD = 1 * 1000;  // Milliseconds to throttle logs inside loops
-constexpr double VALUE_CONSIDERED_ZERO = 1e-10;
+#include "joint_limits/joint_limits_helpers.hpp"
 
 namespace joint_limits
 {
@@ -250,43 +138,11 @@ bool JointSaturationLimiter<JointControlInterfacesData>::on_enforce(
 
   if (desired.has_acceleration())
   {
-    // limiting acc or dec function
-    auto apply_acc_or_dec_limit = [&](const double max_acc_or_dec, double & acc) -> bool
-    {
-      if (std::fabs(acc) > max_acc_or_dec)
-      {
-        acc = std::copysign(max_acc_or_dec, acc);
-        return true;
-      }
-      else
-      {
-        return false;
-      }
-    };
-
-    // check if decelerating - if velocity is changing toward 0
-    double desired_acc = desired.acceleration.value();
-    if (
-      joint_limits.has_deceleration_limits &&
-      ((desired.acceleration.value() < 0 && actual.velocity.has_value() &&
-        actual.velocity.value() > 0) ||
-       (desired.acceleration.value() > 0 && actual.velocity.has_value() &&
-        actual.velocity.value() < 0)))
-    {
-      // limit deceleration
-      limits_enforced =
-        limits_enforced || apply_acc_or_dec_limit(joint_limits.max_deceleration, desired_acc);
-    }
-    else
-    {
-      // limit acceleration (fallback to acceleration if no deceleration limits)
-      if (joint_limits.has_acceleration_limits)
-      {
-        limits_enforced =
-          apply_acc_or_dec_limit(joint_limits.max_acceleration, desired_acc) || limits_enforced;
-      }
-    }
-    desired.acceleration = desired_acc;
+    const auto limits =
+      compute_acceleration_limits(joint_limits, desired.acceleration.value(), actual.velocity);
+    limits_enforced =
+      is_limited(desired.acceleration.value(), limits.first, limits.second) || limits_enforced;
+    desired.acceleration = std::clamp(desired.acceleration.value(), limits.first, limits.second);
   }
 
   if (desired.has_jerk())
