eb357e148adcb4e0a745ac2546a3c11e626b0173
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2024-06-17 10:39:29 +0200

    Parse the actual period to the read and write methods

--- hardware_interface/src/resource_manager.cpp
+++ hardware_interface/src/resource_manager.cpp
@@ -1859,9 +1859,12 @@ HardwareReadWriteStatus ResourceManager::read(
             1.0 / resource_storage_->hardware_info_map_[component.get_name()].read_rate);
           if (
             component.get_last_read_time().seconds() == 0 ||
-            (time - component.get_last_read_time()).seconds() >= hw_read_period.seconds())
+            (time - component.get_last_read_time()) >= hw_read_period)
           {
-            ret_val = component.read(time, hw_read_period);
+            const rclcpp::Duration actual_period = component.get_last_read_time().seconds() == 0
+                                                     ? hw_read_period
+                                                     : time - component.get_last_read_time();
+            ret_val = component.read(time, actual_period);
           }
         }
         const auto component_group = component.get_group_name();
@@ -1936,9 +1939,12 @@ HardwareReadWriteStatus ResourceManager::write(
             1.0 / resource_storage_->hardware_info_map_[component.get_name()].write_rate);
           if (
             component.get_last_write_time().seconds() == 0 ||
-            (time - component.get_last_write_time()).seconds() >= hw_write_period.seconds())
+            (time - component.get_last_write_time()) >= hw_write_period)
           {
-            ret_val = component.write(time, hw_write_period);
+            const rclcpp::Duration actual_period = component.get_last_write_time().seconds() == 0
+                                                     ? hw_write_period
+                                                     : time - component.get_last_read_time();
+            ret_val = component.write(time, actual_period);
           }
         }
         const auto component_group = component.get_group_name();
