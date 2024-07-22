d4540d16d619e010e7aeddcdd1dbf0d1d37f1d6e
Christoph Fröhlich <christophfroehlich@users.noreply.github.com>
2024-07-22 15:11:44 +0200

    Update controller_manager/test/cm.launch.py

--- controller_manager/test/cm.launch.py
+++ controller_manager/test/cm.launch.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Stogl Robotics Consulting UG (haftungsbeschränkt)
+# Copyright (c) 2024 AIT - Austrian Institute of Technology GmbH
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
