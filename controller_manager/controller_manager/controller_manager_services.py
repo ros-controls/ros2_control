90b93b7b301d3d19f5760bb7024986266eeed2ed
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2024-08-05 17:14:09 +0200

    move bcolors class to be reusable

--- controller_manager/controller_manager/controller_manager_services.py
+++ controller_manager/controller_manager/controller_manager_services.py
@@ -29,6 +29,19 @@ import rclpy
 import yaml
 
 
+# from https://stackoverflow.com/a/287944
+class bcolors:
+    MAGENTA = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
 class ServiceNotFoundError(Exception):
     pass
 
