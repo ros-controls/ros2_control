19fc9bb19c5bfd5b982a76b95b03d0007df8c4c6
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2024-08-06 18:03:32 +0200

    Use the current time to trigger the components based on their update rate

--- hardware_interface/src/resource_manager.cpp
+++ hardware_interface/src/resource_manager.cpp
@@ -1857,13 +1857,15 @@ HardwareReadWriteStatus ResourceManager::read(
         {
           const rclcpp::Duration hw_read_period = rclcpp::Duration::from_seconds(
             1.0 / resource_storage_->hardware_info_map_[component.get_name()].read_rate);
+          const auto current_time = resource_storage_->get_clock()->now();
           if (
             component.get_last_read_time().seconds() == 0 ||
-            (time - component.get_last_read_time()) >= hw_read_period)
+            (current_time - component.get_last_read_time()) >= hw_read_period)
           {
-            const rclcpp::Duration actual_period = component.get_last_read_time().seconds() == 0
-                                                     ? hw_read_period
-                                                     : time - component.get_last_read_time();
+            const rclcpp::Duration actual_period =
+              component.get_last_read_time().seconds() == 0
+                ? hw_read_period
+                : current_time - component.get_last_read_time();
             ret_val = component.read(time, actual_period);
           }
         }
@@ -1937,13 +1939,15 @@ HardwareReadWriteStatus ResourceManager::write(
         {
           const rclcpp::Duration hw_write_period = rclcpp::Duration::from_seconds(
             1.0 / resource_storage_->hardware_info_map_[component.get_name()].write_rate);
+          const auto current_time = resource_storage_->get_clock()->now();
           if (
             component.get_last_write_time().seconds() == 0 ||
-            (time - component.get_last_write_time()) >= hw_write_period)
+            (current_time - component.get_last_write_time()) >= hw_write_period)
           {
-            const rclcpp::Duration actual_period = component.get_last_write_time().seconds() == 0
-                                                     ? hw_write_period
-                                                     : time - component.get_last_read_time();
+            const rclcpp::Duration actual_period =
+              component.get_last_write_time().seconds() == 0
+                ? hw_write_period
+                : current_time - component.get_last_write_time();
             ret_val = component.write(time, actual_period);
           }
         }
