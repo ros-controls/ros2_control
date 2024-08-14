ca04b8c457301843df9902e734dbbe507e03b247
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2024-08-14 11:43:27 +0000

    Use entry point instead

--- controller_manager/test/test_spawner_unspawner.cpp
+++ controller_manager/test/test_spawner_unspawner.cpp
@@ -68,13 +68,15 @@ protected:
 
 int call_spawner(const std::string extra_args)
 {
-  std::string spawner_script = "coverage run controller_manager.spawner ";
+  std::string spawner_script =
+    "coverage run $(ros2 pkg prefix controller_manager)/lib/controller_manager/spawner ";
   return std::system((spawner_script + extra_args).c_str());
 }
 
 int call_unspawner(const std::string extra_args)
 {
-  std::string spawner_script = "coverage run controller_manager.unspawner ";
+  std::string spawner_script =
+    "coverage run $(ros2 pkg prefix controller_manager)/lib/controller_manager/unspawner ";
   return std::system((spawner_script + extra_args).c_str());
 }
 
@@ -301,7 +303,7 @@ TEST_F(TestLoadController, unload_on_kill)
   cm_->set_parameter(rclcpp::Parameter("ctrl_3.type", test_controller::TEST_CONTROLLER_CLASS_NAME));
   std::stringstream ss;
   ss << "timeout --signal=INT 5 "
-     << "coverage run controller_manager.spawner "
+     << "python3 -m $(ros2 pkg prefix controller_manager)/lib/controller_manager/spawner "
      << "ctrl_3 -c test_controller_manager --unload-on-kill";
 
   EXPECT_NE(std::system(ss.str().c_str()), 0)
