ed1fbaff900e602810a6b6155f53b711acaede1c
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2024-08-28 18:31:55 +0200

    throw an exception when the components aare initially failed to configure or activate

--- controller_manager/src/controller_manager.cpp
+++ controller_manager/src/controller_manager.cpp
@@ -346,7 +346,13 @@ void ControllerManager::init_resource_manager(const std::string & robot_descript
           RCLCPP_INFO(
             get_logger(), "Setting component '%s' to '%s' state.", component.c_str(),
             state.label().c_str());
-          resource_manager_->set_component_state(component, state);
+          if (
+            resource_manager_->set_component_state(component, state) ==
+            hardware_interface::return_type::ERROR)
+          {
+            throw std::runtime_error(
+              "Failed to set the state of the component : " + component + " to " + state.label());
+          }
           components_to_activate.erase(component);
         }
       }
@@ -370,7 +376,13 @@ void ControllerManager::init_resource_manager(const std::string & robot_descript
   {
     rclcpp_lifecycle::State active_state(
       State::PRIMARY_STATE_ACTIVE, hardware_interface::lifecycle_state_names::ACTIVE);
-    resource_manager_->set_component_state(component, active_state);
+    if (
+      resource_manager_->set_component_state(component, active_state) ==
+      hardware_interface::return_type::ERROR)
+    {
+      throw std::runtime_error(
+        "Failed to set the state of the component : " + component + " to " + active_state.label());
+    }
   }
   robot_description_notification_timer_->cancel();
 }
