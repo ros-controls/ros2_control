0c620be2c4b4463374a503e28a8e497ea2e9679c
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2024-09-25 10:22:25 +0200

    Fix to properly propagate the controller NodeOptions

--- controller_interface/include/controller_interface/controller_interface_base.hpp
+++ controller_interface/include/controller_interface/controller_interface_base.hpp
@@ -185,14 +185,15 @@ public:
   CONTROLLER_INTERFACE_PUBLIC
   virtual rclcpp::NodeOptions define_custom_node_options() const
   {
+    rclcpp::NodeOptions node_options;
 // \note The versions conditioning is added here to support the source-compatibility with Humble
 #if RCLCPP_VERSION_MAJOR >= 21
-    return rclcpp::NodeOptions().enable_logger_service(true);
+    node_options.enable_logger_service(true);
 #else
-    return rclcpp::NodeOptions()
-      .allow_undeclared_parameters(true)
-      .automatically_declare_parameters_from_overrides(true);
+    node_options.allow_undeclared_parameters(true);
+    node_options.automatically_declare_parameters_from_overrides(true);
 #endif
+    return node_options;
   }
 
   /// Declare and initialize a parameter with a type.
