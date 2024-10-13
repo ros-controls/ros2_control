92914066db7ab166b51f7bf3d77c9a4a30f645fb
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2024-10-13 23:35:24 +0200

    Add a add_controller method to controller_manager that accepts ControllerSpec

--- controller_manager/include/controller_manager/controller_manager.hpp
+++ controller_manager/include/controller_manager/controller_manager.hpp
@@ -128,6 +128,12 @@ public:
     return add_controller_impl(controller_spec);
   }
 
+  controller_interface::ControllerInterfaceBaseSharedPtr add_controller(
+    const ControllerSpec & controller_spec)
+  {
+    return add_controller_impl(controller_spec);
+  }
+
   /// configure_controller Configure controller by name calling their "configure" method.
   /**
    * \param[in] controller_name as a string.
