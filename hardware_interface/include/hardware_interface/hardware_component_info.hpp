73dd839fdc80356a130e3dd49e43911cb702c0bb
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2025-01-15 00:00:34 +0100

    Add MovingAverageStatistics struct

--- hardware_interface/include/hardware_interface/hardware_component_info.hpp
+++ hardware_interface/include/hardware_interface/hardware_component_info.hpp
@@ -19,14 +19,57 @@
 #ifndef HARDWARE_INTERFACE__HARDWARE_COMPONENT_INFO_HPP_
 #define HARDWARE_INTERFACE__HARDWARE_COMPONENT_INFO_HPP_
 
+#include <memory>
 #include <string>
 #include <vector>
 
 #include <rclcpp/time.hpp>
+#include "libstatistics_collector/moving_average_statistics/moving_average.hpp"
+#include "libstatistics_collector/moving_average_statistics/types.hpp"
 #include "rclcpp_lifecycle/state.hpp"
+#include "realtime_tools/mutex.hpp"
 
 namespace hardware_interface
 {
+using MovingAverageStatistics =
+  libstatistics_collector::moving_average_statistics::MovingAverageStatistics;
+using StatisticData = libstatistics_collector::moving_average_statistics::StatisticData;
+
+struct MovingAverageStatisticsData
+{
+public:
+  MovingAverageStatisticsData() { statistics = std::make_unique<MovingAverageStatistics>(); }
+
+  void add_measurement(double measurement)
+  {
+    std::unique_lock<realtime_tools::prio_inherit_recursive_mutex> lock(mutex_);
+    statistics->AddMeasurement(measurement);
+    statistics_data = statistics->GetStatistics();
+  }
+
+  void reset()
+  {
+    statistics->Reset();
+    statistics_data = StatisticData();
+  }
+
+  const StatisticData & get_statistics() const
+  {
+    std::unique_lock<realtime_tools::prio_inherit_recursive_mutex> lock(mutex_);
+    return statistics_data;
+  }
+
+private:
+  std::unique_ptr<MovingAverageStatistics> statistics = nullptr;
+  StatisticData statistics_data;
+  mutable realtime_tools::prio_inherit_recursive_mutex mutex_;
+};
+
+struct HardwareComponentStatistics
+{
+  MovingAverageStatisticsData execution_time;
+  MovingAverageStatisticsData periodicity;
+};
 /// Hardware Component Information
 /**
  * This struct contains information about a given hardware component.
@@ -59,6 +102,12 @@ struct HardwareComponentInfo
 
   /// List of provided command interfaces by the component.
   std::vector<std::string> command_interfaces;
+
+  /// Read cycle statistics of the component.
+  std::shared_ptr<HardwareComponentStatistics> read_statistics = nullptr;
+
+  /// Write cycle statistics of the component.
+  std::shared_ptr<HardwareComponentStatistics> write_statistics = nullptr;
 };
 
 }  // namespace hardware_interface
