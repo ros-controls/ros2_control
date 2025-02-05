4c1187541ebdb12afdfeeb4a75600b4933065614
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2025-02-05 15:03:58 +0100

    Improve the prefix name check for the chainable controllers

--- controller_interface/src/chainable_controller_interface.cpp
+++ controller_interface/src/chainable_controller_interface.cpp
@@ -57,7 +57,7 @@ ChainableControllerInterface::export_state_interfaces()
   // check if the names of the controller state interfaces begin with the controller's name
   for (const auto & interface : state_interfaces)
   {
-    if (interface.get_prefix_name() != get_node()->get_name())
+    if (interface.get_prefix_name().find(get_node()->get_name()) == std::string::npos)
     {
       std::string error_msg =
         "The prefix of the interface '" + interface.get_prefix_name() +
@@ -68,7 +68,7 @@ ChainableControllerInterface::export_state_interfaces()
       throw std::runtime_error(error_msg);
     }
     auto state_interface = std::make_shared<hardware_interface::StateInterface>(interface);
-    const auto interface_name = state_interface->get_interface_name();
+    const auto interface_name = state_interface->get_name();
     auto [it, succ] = exported_state_interfaces_.insert({interface_name, state_interface});
     // either we have name duplicate which we want to avoid under all circumstances since interfaces
     // need to be uniquely identify able or something else really went wrong. In any case abort and
@@ -137,7 +137,7 @@ ChainableControllerInterface::export_reference_interfaces()
   const auto ref_interface_size = reference_interfaces.size();
   for (auto & interface : reference_interfaces)
   {
-    if (interface.get_prefix_name() != get_node()->get_name())
+    if (interface.get_prefix_name().find(get_node()->get_name()) == std::string::npos)
     {
       std::string error_msg = "The name of the interface " + interface.get_name() +
                               " does not begin with the controller's name. This is mandatory for "
@@ -149,7 +149,7 @@ ChainableControllerInterface::export_reference_interfaces()
 
     hardware_interface::CommandInterface::SharedPtr reference_interface =
       std::make_shared<hardware_interface::CommandInterface>(std::move(interface));
-    const auto interface_name = reference_interface->get_interface_name();
+    const auto interface_name = reference_interface->get_name();
     // check the exported interface name is unique
     auto [it, succ] = exported_reference_interfaces_.insert({interface_name, reference_interface});
     // either we have name duplicate which we want to avoid under all circumstances since interfaces
