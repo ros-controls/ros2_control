9019be83e429b6308aa0a4b45a9eeceb2722493b
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2025-04-30 14:34:28 +0200

    increase test tolerance to reduce flakiness

--- controller_manager/test/test_controller_manager.cpp
+++ controller_manager/test/test_controller_manager.cpp
@@ -1239,10 +1239,10 @@ TEST_F(TestAsyncControllerUpdateRates, check_the_async_controller_update_rate_an
         testing::AnyOf(testing::Ge(exp_counter - 1), testing::Le(exp_counter)));
       EXPECT_THAT(
         cm_->get_loaded_controllers()[0].periodicity_statistics->Average(),
-        testing::AllOf(testing::Ge(0.95 * exp_periodicity), testing::Lt((1.05 * exp_periodicity))));
+        testing::AllOf(testing::Ge(0.9 * exp_periodicity), testing::Lt((1.05 * exp_periodicity))));
       EXPECT_THAT(
         cm_->get_loaded_controllers()[0].periodicity_statistics->Min(),
-        testing::AllOf(testing::Ge(0.75 * exp_periodicity), testing::Lt((1.2 * exp_periodicity))));
+        testing::AllOf(testing::Ge(0.5 * exp_periodicity), testing::Lt((1.2 * exp_periodicity))));
       EXPECT_THAT(
         cm_->get_loaded_controllers()[0].periodicity_statistics->Max(),
         testing::AllOf(testing::Ge(0.75 * exp_periodicity), testing::Lt((2.0 * exp_periodicity))));
