37e474b7750b8e855eacec76837b9c4dc7f148b1
marqrazz <marq.razz@gmail.com>
2025-05-15 16:02:37 -0600

    add shutdown_on_initial_state_failure parameter

--- controller_manager/src/controller_manager.cpp
+++ controller_manager/src/controller_manager.cpp
@@ -641,9 +641,18 @@ void ControllerManager::init_resource_manager(const std::string & robot_descript
           resource_manager_->set_component_state(component, state) ==
           hardware_interface::return_type::ERROR)
         {
-          RCLCPP_ERROR_STREAM(
-            get_logger(), "Failed to set the initial state of the component : "
-                            << component << " to " << state.label());
+          if (params_->hardware_components_initial_state.shutdown_on_initial_state_failure)
+          {
+            throw std::runtime_error(
+              "Failed to set the initial state of the component : " + component + " to " +
+              state.label());
+          }
+          else
+          {
+            RCLCPP_ERROR_STREAM(
+              get_logger(), "Failed to set the initial state of the component : "
+                              << component << " to " << state.label());
+          }
         }
         components_to_activate.erase(component);
       }
@@ -676,9 +685,18 @@ void ControllerManager::init_resource_manager(const std::string & robot_descript
       resource_manager_->set_component_state(component, active_state) ==
       hardware_interface::return_type::ERROR)
     {
-      RCLCPP_ERROR_STREAM(
-        get_logger(), "Failed to set the initial state of the component : "
-                        << component << " to " << active_state.label());
+      if (params_->hardware_components_initial_state.shutdown_on_initial_state_failure)
+      {
+        throw std::runtime_error(
+          "Failed to set the initial state of the component : " + component + " to " +
+          active_state.label());
+      }
+      else
+      {
+        RCLCPP_ERROR_STREAM(
+          get_logger(), "Failed to set the initial state of the component : "
+                          << component << " to " << active_state.label());
+      }
     }
   }
   robot_description_notification_timer_->cancel();
