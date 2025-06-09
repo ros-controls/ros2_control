72f3253c32a8a119763971106457f21776217234
Soham Patil <sohampatil45939@gmail.com>
2025-06-09 18:55:32 +0530

    Update hardware_interface/include/hardware_interface/actuator_interface.hpp
    Co-authored-by: Sai Kishor Kothakota <saisastra3@gmail.com>

--- hardware_interface/include/hardware_interface/actuator_interface.hpp
+++ hardware_interface/include/hardware_interface/actuator_interface.hpp
@@ -167,7 +167,8 @@ public:
   /// Initialization of the hardware interface from data parsed from the robot's URDF.
   /**
    * \param[in] hardware_info structure with data from URDF.
-   * \param[in] executor weak pointer to the MultiThreaderExecutor used by the controller manager.
+   * \param[in] executor weak pointer to the Executor used by the controller manager.
+   * \warning The parsed executor should not be used to call `cancel()` or use blocking callbacks such as `spin()`.
    * \returns CallbackReturn::SUCCESS if required data are provided and can be parsed.
    * \returns CallbackReturn::ERROR if any error happens or data are missing.
    */
