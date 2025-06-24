5dc1105ac88ad5d1b6e481c8e60d1e9539d4fbc3
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2025-06-24 17:30:08 +0000

    Fix abi::__cxa_demangle for win

--- hardware_interface/include/hardware_interface/handle.hpp
+++ hardware_interface/include/hardware_interface/handle.hpp
@@ -36,6 +36,7 @@
 
 namespace
 {
+#ifndef _WIN32
 template <typename T>
 std::string get_type_name()
 {
@@ -44,6 +45,14 @@ std::string get_type_name()
     abi::__cxa_demangle(typeid(T).name(), nullptr, nullptr, &status), std::free};
   return (status == 0) ? res.get() : typeid(T).name();
 }
+#else
+// not supported on Windows, use typeid directly
+template <typename T>
+std::string get_type_name()
+{
+  return typeid(T).name();
+}
+#endif
 }  // namespace
 
 namespace hardware_interface
