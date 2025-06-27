252c589738bd89fbd19f65d2f33d2585068bbf21
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2025-06-27 09:50:53 +0200

    Fix clang builds

--- hardware_interface/include/hardware_interface/lexical_casts.hpp
+++ hardware_interface/include/hardware_interface/lexical_casts.hpp
@@ -87,11 +87,12 @@ std::vector<T> parse_array(const std::string & array_string)
     }
     else if constexpr (std::is_floating_point_v<T> || std::is_integral_v<T>)
     {
-      if (const T value = static_cast<T>(hardware_interface::stod(value_str)))
+      try
       {
+        const T value = static_cast<T>(hardware_interface::stod(value_str));
         result.push_back(value);
       }
-      else
+      catch (const std::exception &)
       {
         throw std::invalid_argument(
           "Failed converting string to floating point or integer: " + value_str);
