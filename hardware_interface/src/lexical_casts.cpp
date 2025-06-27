3e16c427c7a60a0cd9b11a5f2777985fe7989739
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2025-06-27 10:56:04 +0200

    fix includes

--- hardware_interface/src/lexical_casts.cpp
+++ hardware_interface/src/lexical_casts.cpp
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <iostream>
+#include <algorithm>
+#include <cctype>
 #include <locale>
 #include <optional>
 #include <stdexcept>
