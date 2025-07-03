6fe704c193f59d0628acece6b3d6c8153ee4818c
Christoph Fröhlich <christophfroehlich@users.noreply.github.com>
2025-07-03 09:58:57 +0200

    Fix conflicts

--- hardware_interface/include/hardware_interface/handle.hpp
+++ hardware_interface/include/hardware_interface/handle.hpp
@@ -19,31 +19,7 @@
 #include <utility>
 
 #include "hardware_interface/macros.hpp"
-<<<<<<< HEAD
 #include "hardware_interface/visibility_control.h"
-=======
-
-namespace
-{
-#ifndef _WIN32
-template <typename T>
-std::string get_type_name()
-{
-  int status = 0;
-  std::unique_ptr<char[], void (*)(void *)> res{
-    abi::__cxa_demangle(typeid(T).name(), nullptr, nullptr, &status), std::free};
-  return (status == 0) ? res.get() : typeid(T).name();
-}
-#else
-// not supported on Windows, use typeid directly
-template <typename T>
-std::string get_type_name()
-{
-  return typeid(T).name();
-}
-#endif
-}  // namespace
->>>>>>> b87334a (Add pixi workflow and dependency file (#2338))
 
 namespace hardware_interface
 {
