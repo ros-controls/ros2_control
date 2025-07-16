de1d00bfaf8d8e6d7c9f06de62245b3074bfbced
Soham Patil <sohampatil45939@gmail.com>
2025-07-16 19:48:26 +0530

    Addition of a Default Node for Hardware Component (#2348)
    (cherry picked from commit d3762542e9cc1fd570d0eb003a978b8ab93b7661)

--- hardware_interface/include/hardware_interface/sensor_interface.hpp
+++ hardware_interface/include/hardware_interface/sensor_interface.hpp
@@ -166,6 +166,26 @@ public:
         info_.thread_priority);
       read_async_handler_->start_thread();
     }
+
+    if (auto locked_executor = params.executor.lock())
+    {
+      std::string node_name = params.hardware_info.name;
+      std::transform(
+        node_name.begin(), node_name.end(), node_name.begin(),
+        [](unsigned char c) { return std::tolower(c); });
+      std::replace(node_name.begin(), node_name.end(), '/', '_');
+      hardware_component_node_ = std::make_shared<rclcpp::Node>(node_name);
+      locked_executor->add_node(hardware_component_node_->get_node_base_interface());
+    }
+    else
+    {
+      RCLCPP_WARN(
+        params.logger,
+        "Executor is not available during hardware component initialization for '%s'. Skipping "
+        "node creation!",
+        params.hardware_info.name.c_str());
+    }
+
     hardware_interface::HardwareComponentInterfaceParams interface_params;
     interface_params.hardware_info = info_;
     interface_params.executor = params.executor;
@@ -436,6 +456,12 @@ public:
    */
   rclcpp::Clock::SharedPtr get_clock() const { return sensor_clock_; }
 
+  /// Get the default node of the ActuatorInterface.
+  /**
+   * \return node of the ActuatorInterface.
+   */
+  rclcpp::Node::SharedPtr get_node() const { return hardware_component_node_; }
+
   /// Get the hardware info of the SensorInterface.
   /**
    * \return hardware info of the SensorInterface.
@@ -476,6 +502,7 @@ protected:
 private:
   rclcpp::Clock::SharedPtr sensor_clock_;
   rclcpp::Logger sensor_logger_;
+  rclcpp::Node::SharedPtr hardware_component_node_ = nullptr;
   // interface names to Handle accessed through getters/setters
   std::unordered_map<std::string, StateInterface::SharedPtr> sensor_states_map_;
 
