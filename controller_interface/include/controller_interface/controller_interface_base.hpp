d2e0b2ea2478ece29cc9504ee13fc0e732f02acf
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2025-07-22 08:31:06 +0000

    Fix documentation

--- controller_interface/include/controller_interface/controller_interface_base.hpp
+++ controller_interface/include/controller_interface/controller_interface_base.hpp
@@ -328,17 +328,12 @@ public:
   void enable_introspection(bool enable);
 
 protected:
-  /** Loaned command interfaces. The order of the interfaces is the same as exported in the
-   * \ref ActuatorInterface::export_command_interfaces "export_command_interfaces"
-   * \ref SystemInterface::export_command_interfaces "export_command_interfaces"
-   * methods, or defined by the \ref command_interface_configuration() if auto-export is used.
+  /** Loaned command interfaces. The order of the interfaces is the same as defined in
+   * \ref command_interface_configuration() if interface_configuration_type::INDIVIDUAL is set.
    */
   std::vector<hardware_interface::LoanedCommandInterface> command_interfaces_;
-  /** Loaned state interfaces. The order of the interfaces is the same as exported in the
-   * \ref ActuatorInterface::export_state_interfaces "export_state_interfaces"
-   * \ref SensorInterface::export_state_interfaces "export_state_interfaces"
-   * \ref SystemInterface::export_state_interfaces "export_state_interfaces"
-   * methods, or defined by the \ref state_interface_configuration() if auto-export is used.
+  /** Loaned state interfaces. The order of the interfaces is the same as defined in
+   * \ref state_interface_configuration() if interface_configuration_type::INDIVIDUAL is set.
    */
   std::vector<hardware_interface::LoanedStateInterface> state_interfaces_;
 
