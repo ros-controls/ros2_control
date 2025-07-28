b530bc958a09786e12ab007c51cf0f6cf76f291a
Bence Magyar <bence.magyar.robotics@gmail.com>
2025-07-28 23:24:44 +0100

    Update test_resource_manager_prepare_perform_switch.cpp

--- hardware_interface_testing/test/test_resource_manager_prepare_perform_switch.cpp
+++ hardware_interface_testing/test/test_resource_manager_prepare_perform_switch.cpp
@@ -210,7 +210,7 @@ TEST_F(
   EXPECT_EQ(claimed_system_acceleration_state_->get_optional().value(), 303.0);
   EXPECT_EQ(claimed_actuator_position_state_->get_optional().value(), 0.0);
 
-  // When TestActuatorHardware is INACTIVE expect OK
+  // When TestActuatorHardware is INACTIVE expect not OK
   EXPECT_FALSE(rm_->prepare_command_mode_switch(legal_keys_actuator, legal_keys_actuator));
   EXPECT_EQ(claimed_system_acceleration_state_->get_optional().value(), 303.0);
   EXPECT_NEAR(claimed_actuator_position_state_->get_optional().value(), 0.0, 1e-7)
