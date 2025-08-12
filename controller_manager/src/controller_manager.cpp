50928ec3b8c70b7d3788bd1641697e62bb200004
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2025-08-12 13:29:08 +0200

    Set back the original load_and_initialize_components

--- controller_manager/src/controller_manager.cpp
+++ controller_manager/src/controller_manager.cpp
@@ -635,7 +635,13 @@ void ControllerManager::init_resource_manager(const std::string & robot_descript
   {
     resource_manager_->import_joint_limiters(robot_description_);
   }
-  if (!resource_manager_->load_and_initialize_components(robot_description, update_rate_))
+  hardware_interface::ResourceManagerParams params;
+  params.robot_description = robot_description;
+  params.clock = trigger_clock_;
+  params.logger = this->get_logger();
+  params.executor = executor_;
+  params.update_rate = static_cast<unsigned int>(params_->update_rate);
+  if (!resource_manager_->load_and_initialize_components(params))
   {
     RCLCPP_WARN(
       get_logger(),
