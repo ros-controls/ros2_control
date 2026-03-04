346668eacb79b8f27ec80b57777cdece57c64554
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2026-03-04 10:53:57 +0100

    [Spawner] Block further SIGINTs with unload_on_kill option (#3075)

--- controller_manager/controller_manager/spawner.py
+++ controller_manager/controller_manager/spawner.py
@@ -16,6 +16,7 @@
 import argparse
 import errno
 import os
+import signal
 import sys
 import time
 import warnings
@@ -310,6 +311,11 @@ def main(args=None):
             while True:
                 time.sleep(1)
         except KeyboardInterrupt:
+            # Ignore further SIGINTs so a second signal cannot interrupt cleanup.
+            # Without this, a signal delivered while rclpy's C extension returns to
+            # Python can raise a second KeyboardInterrupt inside the except block,
+            # skipping the deactivate/unload calls and leaving controllers loaded.
+            signal.signal(signal.SIGINT, signal.SIG_IGN)
             if not args.stopped and not args.inactive:
                 node.get_logger().info("Interrupt captured, deactivating and unloading controller")
                 # TODO(saikishor) we might have an issue in future, if any of these controllers is in chained mode
