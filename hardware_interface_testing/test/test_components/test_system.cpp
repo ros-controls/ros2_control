303a07c892b09e859f2c45069ad825b7cb78521a
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2026-03-11 20:04:57 +0100

    add tests to verify the functionality

--- hardware_interface_testing/test/test_components/test_system.cpp
+++ hardware_interface_testing/test/test_components/test_system.cpp
@@ -144,6 +144,11 @@ class TestSystem : public SystemInterface
     {
       return return_type::DEACTIVATE;
     }
+    // simulate exception on read
+    if (velocity_command_[0] == test_constants::READ_THROW_VALUE)
+    {
+      throw std::runtime_error("Exception from TestSystem::read() as requested.");
+    }
     // The next line is for the testing purposes. We need value to be changed to
     // be sure that the feedback from hardware to controllers in the chain is
     // working as it should. This makes value checks clearer and confirms there
@@ -173,6 +178,11 @@ class TestSystem : public SystemInterface
     if (velocity_command_[0] == test_constants::WRITE_DEACTIVATE_VALUE)
     {
       return return_type::DEACTIVATE;
+    }
+    // simulate exception on write
+    if (velocity_command_[0] == test_constants::WRITE_THROW_VALUE)
+    {
+      throw std::runtime_error("Exception from TestSystem::write() as requested.");
     }
     return return_type::OK;
   }
