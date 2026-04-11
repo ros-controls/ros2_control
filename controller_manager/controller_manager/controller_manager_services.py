834db0cc0a158c45a02d8a76036663d41557c072
shlok-mehndiratta <shlokm23@iiserb.ac.in>
2026-04-12 02:01:33 +0530

    fix: restore legacy ros2param import fallback for Humble CI jobs
    Signed-off-by: shlok-mehndiratta <shlokm23@iiserb.ac.in>

--- controller_manager/controller_manager/controller_manager_services.py
+++ controller_manager/controller_manager/controller_manager_services.py
@@ -30,7 +30,12 @@ import rclpy
 import yaml
 from rcl_interfaces.msg import Parameter
 
-from rclpy.parameter import get_parameter_value
+# @note: The versions conditioning is added here to support the source-compatibility with Humble
+# The `get_parameter_value` function is moved to `rclpy.parameter` module from `ros2param.api` module from version 3.6.0
+try:
+    from rclpy.parameter import get_parameter_value
+except ImportError:
+    from ros2param.api import get_parameter_value
 from rcl_interfaces.srv import SetParameters
 
 
