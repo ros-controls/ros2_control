763310b8b80c411ff092479b184c9ffe4fd8c801
Sachin Kumar <sachinkum123567@gmail.com>
2026-06-24 13:04:52 +0200

    Enhance documentation for clamp_joint_limits and handle_braking_near_position_limit methods with detailed step-by-step explanations
    Signed-off-by: Sachin Kumar <sachinkum123567@gmail.com>

--- joint_limits/include/joint_limits/joint_saturation_limiter.hpp
+++ joint_limits/include/joint_limits/joint_saturation_limiter.hpp
@@ -73,6 +73,11 @@ public:
    * velocity and finally acceleration.
    * The method support partial existence of limits, e.g., missing position limits for continuous
    * joins.
+   * 
+   * Steps:
+   * 1. clamp joint limits -> update braking_near_position_limit_triggered
+   * 2. if braking_near_position_limit_triggered -> handle_braking_near_position_limit
+   * 3. update desired_joint_states for position, velocity, accleration
    *
    * \param[in] current_joint_states current joint states a robot is in.
    * \param[in,out] desired_joint_states joint state that should be adjusted to obey the limits.
@@ -117,12 +122,33 @@ private:
   /**
    * @brief
    * Clamps the joint limits
+   * 
+   * Steps:
+   * 1. if has_desired_position -> has_position_limits -> clamp pos -> update desired_vel_
+   * 2. has_velocity_limits -> if desired_vel not defined -> calc from (desired_pos - current_position) . dt ->
+   * clamp vel -> if has_desired_position -> recalculate desired_pos -> calc desired_acc from vel
+   * 3. if has_acc_limits || has_dec_limits -> if desired_acc not defined -> cal from (desired_vel - current_vel) / dt
+   * -> if decelerating -> limit deceleration -> if accelerating -> limit acceleration -> if limit_applied -> 
+   * cal desired_vel from current_joint_vel + desried_acceleration . dt -> if has_desired_positon -> calc desired_pos
+   * 
+   * if has_position_limits -> if has_desired_velosity && !has_desired_position -> calc expected_pos -> clamp pos -> if pos != expected_pos -> expected_pos = pos -> calc desired_vel
+   * 
+   * stopping_deccel = desired_vel / dt -> update stopping_deccel based on max_dec or mac_acc -> cal stopping_distance -> cal stopping_duration -> check if joint limits are beyond stopping_dist and desired_velocity -> else distance_after_stopping_duration -> recheck if jont limits are beyond -> update the bracking_near_pos_limit_trigger
+   * 
    */
   void clamp_joint_limits(const bool has_desired_position, const bool has_desired_velocity, const bool has_desired_acceleration, const trajectory_msgs::msg::JointTrajectoryPoint & current_joint_states, trajectory_msgs::msg::JointTrajectoryPoint & desired_joint_states, bool & limits_enforced, const std::vector<double> current_joint_velocities, bool & braking_near_position_limit_triggered,  const double dt_seconds);
 
   /**
    * @brief
    * Handles the braking near position limit
+   * 
+   * Steps:
+   * for every joint -> cal desired_acc -> limit deceleration -> limit acceleration 
+   * 
+   * if has_desired_velocity -> update desired_vel = current_joint_velocity + desired_acc . dt 
+   * 
+   * if has_desired_position -> updated desired_pos = current_joint_position + current_joint_velocities . dt + 0.5 * desired_acc . dt . dt
+   * 
    */
   void handle_braking_near_position_limit(
     const std::vector<double> & current_joint_velocities, double dt_seconds,
