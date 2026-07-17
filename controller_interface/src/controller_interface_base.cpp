2014c5763fe4ae8fc6e66a0a8eb2fba88e8ae075
nibanovic <nikola.banovic@b-robotized.com>
2026-07-17 14:30:51 +0200

    add missing controller async parameter declarations

--- controller_interface/src/controller_interface_base.cpp
+++ controller_interface/src/controller_interface_base.cpp
@@ -100,6 +100,13 @@ return_type ControllerInterfaceBase::init(
     auto_declare<int>("update_rate", static_cast<int>(params.controller_manager_update_rate));
     auto_declare<bool>("is_async", false);
     auto_declare<int>("thread_priority", -100);
+
+    auto_declare<int>("async_parameters.thread_priority", 50);
+    auto_declare<std::string>("async_parameters.scheduling_policy", "synchronized");
+    auto_declare<std::vector<int64_t>>("async_parameters.cpu_affinity", std::vector<int64_t>());
+    auto_declare<int>("async_parameters.execution_rate", impl_->ctrl_itf_params_.update_rate);
+    auto_declare<bool>("async_parameters.wait_until_initial_trigger", true);
+    auto_declare<bool>("async_parameters.print_warnings", true);
   }
   catch (const std::exception & e)
   {
