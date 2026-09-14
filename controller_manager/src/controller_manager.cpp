dd4dbbd4e763c1a7e1967824dde6b15f8abe065b
渠超胜 <quchaosheng000406@163.com>
2026-09-15 05:49:34 +0800

    Fix data race in overrun warning parameter (#3619)
    (cherry picked from commit 17e2c1a85452c94e9024c62ca097e37f3a27e2fc)

--- controller_manager/src/controller_manager.cpp
+++ controller_manager/src/controller_manager.cpp
@@ -732,6 +732,7 @@ void ControllerManager::initialize_parameters()
     cm_param_listener_ = std::make_shared<controller_manager::ParamListener>(
       this->get_node_parameters_interface(), this->get_logger());
     params_ = std::make_shared<controller_manager::Params>(cm_param_listener_->get_params());
+    overruns_print_warnings_.store(params_->overruns.print_warnings, std::memory_order_relaxed);
     update_rate_ = static_cast<unsigned int>(params_->update_rate);
     trigger_clock_ =
       use_sim_time_ ? this->get_clock() : std::make_shared<rclcpp::Clock>(RCL_STEADY_TIME);
@@ -933,6 +934,7 @@ void ControllerManager::set_initial_hardware_components_state()
   if (cm_param_listener_->is_old(*params_))
   {
     *params_ = cm_param_listener_->get_params();
+    overruns_print_warnings_.store(params_->overruns.print_warnings, std::memory_order_relaxed);
   }
 
   // unconfigured (loaded only)
@@ -3680,7 +3682,9 @@ void ControllerManager::write(const rclcpp::Time & time, const rclcpp::Duration 
   execution_time_.total_time =
     execution_time_.write_time + execution_time_.update_time + execution_time_.read_time;
   const double expected_cycle_time = 1.e6 / static_cast<double>(get_update_rate());
-  if (params_->overruns.print_warnings && execution_time_.total_time > expected_cycle_time)
+  if (
+    overruns_print_warnings_.load(std::memory_order_relaxed) &&
+    execution_time_.total_time > expected_cycle_time)
   {
     if (execution_time_.switch_time > 0.0)
     {
@@ -4439,6 +4443,7 @@ void ControllerManager::controller_activity_diagnostic_callback(
   if (cm_param_listener_->is_old(*params_))
   {
     *params_ = cm_param_listener_->get_params();
+    overruns_print_warnings_.store(params_->overruns.print_warnings, std::memory_order_relaxed);
   }
 
   auto make_stats_string =
@@ -4618,6 +4623,7 @@ void ControllerManager::hardware_components_diagnostic_callback(
   if (cm_param_listener_->is_old(*params_))
   {
     *params_ = cm_param_listener_->get_params();
+    overruns_print_warnings_.store(params_->overruns.print_warnings, std::memory_order_relaxed);
   }
 
   auto make_stats_string =
